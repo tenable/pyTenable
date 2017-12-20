4ab62c61c80768a36b65ba5accef0bfe1350480e
Steven McGrath <smcgrath@tenable.com>
2017-12-20 10:22:45 -0600

    Agent COnfig and Agent Exclusions

---
+++ tenable/tenable_io/agent_config.py
@@ -0,0 +1,58 @@
+from .base import APIEndpoint
+from .utils import checktype
+
+class AgentConfigAPI(APIEndpoint):
+    def edit(self, scanner_id, software_update=None, auto_unlink=None, 
+             unlink_exp=None):
+        '''
+        agent-config: edit
+        https://cloud.tenable.com/api#/resources/agent-config/edit
+
+        Args:
+            scanner_id (int): The scanner ID.
+            software_update (:obj:`bool`, optional): 
+                If True, software updates are enabled for agents (exclusions may
+                override this).  If false, software updates for all agents are
+                disabled.
+            auto_unlink (:obj:`bool`, optional):
+                If true, agent auto-unlinking is enabled, allowing agents to
+                automatically unlink themselves after a given period of time.
+            unkink_exp (:obj:`int`, optional):
+                The expiration time for agents to auto-unlink in days. Agents
+                will be removed from Tenable.io automatically after the
+                inactivity age of the agent crosses this threshold.  Valid
+                values are between 1 and 365.
+
+        Returns:
+            dict: Dictionary of the applied settings is returned if successfully
+                applied.
+        '''
+        checktype(scanner_id, int)
+
+        # Lets build the dictionary that we will present to the API...
+        payload = {'auto_unlink': {}}
+        if checktype(software_update, bool):
+            payload['software_update'] = software_update
+        if checktype(auto_unlink, bool):
+            payload['auto_unlink']['enabled'] = auto_unlink
+        if checktype(unlink_exp, int, range(1, 366)):
+            payload['auto_unlink']['expiration'] = unlink_exp
+
+        # Now to run the API call and get the response
+        return self._api.put(
+            'scanners/{}/agents/config'.format(scanner_id), json=payload).json()
+
+    def details(self, scanner_id):
+        '''
+        agent-config: details
+        https://cloud.tenable.com/api#/resources/agent-config/details
+
+        Args:
+            scanner_id (int): The scanner ID.
+
+        Returns:
+            dict: Dictionary of the current settings.
+        '''
+        checktype(scanner_id, int)
+        return self._api.get(
+            'scanners/{}/agents/config'.format(scanner_id)).json()
---
+++ tenable/tenable_io/agent_exclusions.py
@@ -0,0 +1,185 @@
+from .base import APIEndpoint
+from .utils import checktype
+from .models import ExclusionSchedule
+
+class AgentExclusionsAPI(APIEndpoint):
+    def create(self, scanner_id, name, starttime=None, endtime=None, 
+               timezone=None, description=None, freq=None, 
+               interval=None, weekdays=None, dayofmonth=None,
+               enabled=None, sched_obj=None):
+        '''
+        agent-exclusions: create
+        https://cloud.tenable.com/api#/resources/agent-exclusions/create
+
+        Args:
+            scanner_id (int): The scanner id.
+            name (str): The name of the exclusion to create.
+            description (str, optional): 
+                Some further detail about the exclusion.
+            starttime (datetime, optional): When the exclusion should start.
+            endtime (datetime, optional): When the exclusion should end.
+            timezone (str, optional): 
+                The timezone to use for the exclusion.  The default if none is 
+                specified is to use UTC.
+            freq (str, optional):
+                The frequency of the rule. The string inputted will be upcased.
+                Valid values are: *ONETIME, DAILY, WEEKLY, MONTHLY, YEARLY*.
+            interval (int, optional): The interval of the rule.
+            weekdays (list, optional):
+                List of 2-character representations of the days of the week to
+                repeate the frequency rule on.  Valied values are:
+                *SU, MO, TU, WE, TH, FR, SA*
+                Default values: ``['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA']``
+            dayofmonth (int, optional):
+                The day of the month to repeat a **MONTHLY** frequency rule on.
+            sched_obj (obj:`ExclusionSchedule`, optional):
+                Instead of supplying *any* of the optional parameters, a
+                ExclusionSchedule object with the appropriate values may instead 
+                be passed.  This will bypass interpretation of any of the other
+                paramters and use the object instead.
+
+        Returns:
+            dict: Dictionary of the newly minted exclusion. 
+        '''
+
+        # If an ExclusionSchedule object was not passed in, then we will have to
+        # construct one using all of the available parameters.
+        if not sched_obj and (starttime or endtime or timezone or freq 
+          or interval or weekdays or dayofmonth or enabled):
+            sched_obj = ExclusionSchedule(
+                starttime=starttime, 
+                endtime=endtime,
+                timezone=timezone,
+                enabled=enabled,
+                freq=freq,
+                interval=interval,
+                weekdays=weekdays,
+                dayofmonth=dayofmonth
+            )
+
+        # Lets start constructing the payload to be sent to the API...
+        payload = {'name': checktype(name, str)}
+
+        # As the description and schedule are optional, we will only add 
+        # them into the payload if one was specified.
+        if checktype(description, str):
+            payload['description'] = description
+        if sched_obj:
+            payload['schedule'] = sched_obj.json()
+
+        # Lests check to make sure that the scanner_id is an integer as the API
+        # documentation requests and if we don't raise an error, then lets make
+        # the call.
+        return self._api.post(
+            'scanners/{}/agents/exclusions'.format(checktype(scanner_id, int)), json=payload)
+
+    def delete(self, scanner_id, exclusion_id):
+        '''
+        agent-exclusions: delete
+        https://cloud.tenable.com/api#/resources/agent-exclusions/delete
+
+        Args:
+            scanner_id (int): The id of the scanner
+            exclusion_id (int): The id of the exclusion object in Tenable.io
+
+        Returns:
+            Unknown.
+        '''
+        return self._api.delete(
+            'scanners/{}/agents/exclusions/{}'.format(
+                checktype(scanner_id, int),
+                checktype(exclusion_id, int)
+            ))
+
+    def details(self, scanner_id, exclusion_id):
+        '''
+        agent-exclusion: details
+        https://cloud.tenable.com/api#/resources/agent-exclusions/details
+
+        Args:
+            scanner_id (int): The id of the scanner
+            exclusion_id (int): The id of the exclusion object in Tenable.io
+
+        Returns:
+            dict: The exclusion resource dictionary.
+        '''
+        return self._api.delete(
+            'scanners/{}/agents/exclusions/{}'.format(
+                checktype(scanner_id, int),
+                checktype(exclusion_id, int)
+            ))
+
+    def edit(self, scanner_id, exclusion_id, name=None, starttime=None, 
+            endtime=None, timezone=None, description=None, freq=None, 
+            interval=None, weekdays=None, dayofmonth=None, enabled=None, 
+            sched_obj=None):
+        '''
+        Args:
+            scanner_id (int): The scanner id.
+            exclusion_id (int): The id of the exclusion object in Tenable.io
+            name (str, optional): The name of the exclusion to create.
+            description (str, optional): 
+                Some further detail about the exclusion.
+            starttime (datetime, optional): When the exclusion should start.
+            endtime (datetime, optional): When the exclusion should end.
+            timezone (str, optional): 
+                The timezone to use for the exclusion.  The default if none is 
+                specified is to use UTC.
+            freq (str, optional):
+                The frequency of the rule. The string inputted will be upcased.
+                Valid values are: *ONETIME, DAILY, WEEKLY, MONTHLY, YEARLY*.
+            interval (int, optional): The interval of the rule.
+            weekdays (list, optional):
+                List of 2-character representations of the days of the week to
+                repeate the frequency rule on.  Valied values are:
+                *SU, MO, TU, WE, TH, FR, SA*
+                Default values: ``['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA']``
+            dayofmonth (int, optional):
+                The day of the month to repeat a **MONTHLY** frequency rule on.
+            sched_obj (obj:`ExclusionSchedule`, optional):
+                Instead of supplying *any* of the optional parameters, a
+                ExclusionSchedule object with the appropriate values may instead 
+                be passed.  This will bypass interpretation of any of the other
+                paramters and use the object instead.
+
+        Returns:
+            dict: Dictionary of the newly minted exclusion. 
+        '''
+
+        # If an ExclusionSchedule object was not passed in, then we will have to
+        # construct one using all of the available parameters.
+        if not sched_obj and (starttime or endtime or timezone or freq 
+          or interval or weekdays or dayofmonth or enabled):
+            sched_obj = ExclusionSchedule(
+                starttime=starttime, 
+                endtime=endtime,
+                timezone=timezone,
+                enabled=enabled,
+                freq=freq,
+                interval=interval,
+                weekdays=weekdays,
+                dayofmonth=dayofmonth
+            )
+
+        # Lets start constructing the payload to be sent to the API...
+        payload = {'name': checktype(name, str)}
+
+        # As the description, name, and schedule are optional, we will only add 
+        # them into the payload if one was specified.
+        if checktype(description, str):
+            payload['description'] = description
+        if checktype(name, str):
+            payload['name'] = name
+        if sched_obj:
+            payload['schedule'] = sched_obj.json()
+
+        # Lests check to make sure that the scanner_id  and exclusion_id are 
+        # integers as the API documentation requests and if we don't raise an 
+        # error, then lets make the call.
+        return self._api.put(
+            'scanners/{}/agents/exclusions/{}'.format(
+                checktype(scanner_id, int),
+                checktype(exclusion_id, int)
+        ), json=payload)
+
+
