891413e63705d19c57e787926a4919bee2524f2e
Steven McGrath <steve@chigeek.com>
2018-02-12 10:45:43 -0600

    modified TenableIO APISession to throw warnings

--- tenable/tenable_io/__init__.py
+++ tenable/tenable_io/__init__.py
@@ -1,22 +1,26 @@
-from tenable.tenable_io.agent_config import AgentConfigAPI
-from tenable.tenable_io.agent_exclusions import AgentExclusionsAPI
+from tenable.tenable_io.agents import AgentsAPI
 from tenable.base import APISession
+
+import warnings
 
 class TenableIO(APISession):
     URL = 'https://cloud.tenable.com'
     def __init__(access_key, secret_key, url=None, retries=None, backoff=None):
         '''
         '''
+
+        warnings.warn('Incomplete and/or Nonworking Code May exist')
+
         self._access_key = access_key
         self._secret_key = secret_key
         APISession.__init__(self, url, retries, backoff)
 
         # Graft on the API Components
-        self.agent_config = AgentConfigAPI(self)
-        self.agent_exclusions = AgentExclusionsAPI(self)
+        self.agents = AgentsAPI(self)
 
-    def build_session(self):
+    def _build_session(self):
         '''
+        Build the session and add the API Keys into the session
         '''
         APISession.build_session(self)
         self._session.headers.update({
