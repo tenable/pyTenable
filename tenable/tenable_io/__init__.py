f2029b6fd37abf2aed29d4b857495f6c9aae3b1b
Steven McGrath <steve@chigeek.com>
2018-02-14 16:06:23 -0600

    Removed the warning and adjusted build_session to _build_session

--- tenable/tenable_io/__init__.py
+++ tenable/tenable_io/__init__.py
@@ -1,15 +1,12 @@
 from tenable.tenable_io.agents import AgentsAPI
 from tenable.base import APISession
 
-import warnings
 
 class TenableIO(APISession):
     URL = 'https://cloud.tenable.com'
-    def __init__(access_key, secret_key, url=None, retries=None, backoff=None):
+    def __init__(self, access_key, secret_key, url=None, retries=None, backoff=None):
         '''
         '''
-
-        warnings.warn('Incomplete and/or Nonworking Code May exist')
 
         self._access_key = access_key
         self._secret_key = secret_key
@@ -22,7 +19,7 @@ class TenableIO(APISession):
         '''
         Build the session and add the API Keys into the session
         '''
-        APISession.build_session(self)
+        APISession._build_session(self)
         self._session.headers.update({
             'X-APIKeys': 'accessKey={}; secretKey={};'.format(
                 self._access_key, self._secret_key)
