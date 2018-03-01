18455eda2fc04e4d8f4c0a8b5d7bcb86948ff6fd
Steven McGrath <steve@chigeek.com>
2018-03-01 07:03:24 -0600

    TIOIterator (based off of the generic one) that adds in the page handing that Tenable.io uses.

---
+++ tenable/tenable_io/base.py
@@ -0,0 +1,32 @@
+from tenable.base import APIResultsIterator
+
+class TIOIterator(APIResultsIterator):
+    def _get_data(self):
+        '''
+        Request the next page of data
+        '''
+        return None, None
+
+    def _get_page(self):
+        '''
+        Get the next page of records
+        '''
+        # First we need to see if there is a page limit and if there is, have
+        # we run into that limit.  If we have, then return a StopIteration
+        # exception.
+        if self._pages_total and self._pages_requested >= self._pages_total:
+            raise StopIteration()
+
+        # Lets make the actual call at this point.
+        resp, key = self._get_data()
+
+        # Now that we have the response, lets reset any counters we need to, 
+        # and increment things like the page counter, offset, etc.
+        self.page_count = 0
+        self._pages_requested += 1
+        self._offset += self._limit
+
+        # Lastly we want to refresh the page data and the total based on the
+        # most recent data we have.
+        self.page = resp[key]
+        self.total = resp['pagination']['total']
