bb1dbd30fa33145c61f5e06b2f8da189e79fd70e
Steven McGrath <steve@chigeek.com>
2018-11-01 12:40:43 -0500

    inlined assets API docs. #25

--- tenable/io/assets.py
+++ tenable/io/assets.py
@@ -1,17 +1,44 @@
+'''
+assets
+======
+
+The following methods allow for interaction into the Tenable.io 
+`assets <https://cloud.tenable.com/api#/resources/assets>`_ 
+API endpoints.
+
+Methods available on ``tio.assets``:
+
+.. rst-class:: hide-signature
+.. autoclass:: AssetsAPI
+
+    .. automethod:: asset_import
+    .. automethod:: details
+    .. automethod:: import_job_details
+    .. automethod:: list
+    .. automethod:: list_import_jobs
+'''
 from .base import TIOEndpoint
 
 class AssetsAPI(TIOEndpoint):
     def list(self):
         '''
+        Returns a list of assets.
+
         `assets: list-assets <https://cloud.tenable.com/api#/resources/assets/list-assets>`_
 
         Returns:
             list: List of asset records.
+
+        Examples:
+            >>> for asset in tio.assets.list():
+            ...     pprint(asset)
         '''
         return self._api.get('assets').json()['assets']
 
-    def info(self, uuid):
+    def details(self, uuid):
         '''
+        Retrieves the details about a specific asset.
+
         `assets: asset-info <https://cloud.tenable.com/api#/resources/assets/asset-info>`_
 
         Args:
@@ -20,14 +47,19 @@ class AssetsAPI(TIOEndpoint):
 
         Returns:
             dict: Asset resource definition.
+
+        Examples:
+            >>> asset = tio.assets.details('00000000-0000-0000-0000-000000000000')
         '''
         return self._api.get(
             'assets/{}'.format(
                 self._check('uuid', uuid, str)
             )).json()
 
-    def asset_import(self, definitions, source):
+    def asset_import(self, source, *assets):
         '''
+        Imports asset information into Tenable.io from an external source.
+
         `assets: import <https://cloud.tenable.com/api#/resources/assets/import>`_
 
         Imports a list of asset definition dictionaries.  Each asset record must
@@ -37,13 +69,21 @@ class AssetsAPI(TIOEndpoint):
         documentation.
 
         Args:
-            definitions (list):
+            *assets (list):
                 The list of asset dictionaries
             source (str):
                 An identifier to be used to upload the assets.
 
         Returns:
             str: The job UUID.
+
+        Examples:
+            >>> tio.assets.asset_import('example_source', {
+            ...     'fqdn': ['example.py.test'], 
+            ...     'ipv4': ['192.168.254.1'], 
+            ...     'netbios_name': 'example', 
+            ...     'mac_address': ['00:00:00:00:00:00']
+            ... })
 
         .. _asset resource:
             https://cloud.tenable.com/api#/resources/assets
@@ -53,21 +93,29 @@ class AssetsAPI(TIOEndpoint):
         # should suffice for now.
         return self._api.post(
             'import/assets', json={
-                'assets': self._check('definitions', definitions, list),
+                'assets': [self._check('asset', i, dict) for i in assets],
                 'source': self._check('source', source, str)
             }).json()['asset_import_job_uuid']
 
-    def import_jobs(self):
+    def list_import_jobs(self):
         '''
+        Returns a list of asset import jobs.
+
         `assets: list-import-jobs <https://cloud.tenable.com/api#/resources/assets/list-import-jobs>`_
 
         Returns:
             list: list of job records.
+
+        Examples:
+            >>> for job in tio.assets.list_import_jobs():
+            ...     pprint(job)
         '''
         return self._api.get('import/asset-jobs').json()['asset_import_jobs']
 
-    def import_job_info(self, uuid):
+    def import_job_details(self, uuid):
         '''
+        Returns the details about a specific asset import job.
+
         `assets: import-job-info <https://cloud.tenable.com/api#/resources/assets/import-job-info>`_
 
         uuid (str):
@@ -75,6 +123,10 @@ class AssetsAPI(TIOEndpoint):
 
         Returns:
             dict: The job Resource record.
+
+        Examples:
+            >>> job = tio.assets.import_job_details('00000000-0000-0000-0000-000000000000')
+            >>> pprint(job)
         '''
         return self._api.get(
             'import/asset-jobs/{}'.format(
