499cfd7341d6c387fd55ff0a307e3b353eafc309
Steven McGrath <steve@chigeek.com>
2018-11-01 14:35:25 -0500

    tested outputs of plugins #33

--- tests/io/test_plugins.py
+++ tests/io/test_plugins.py
@@ -2,20 +2,40 @@ from .fixtures import *
 from tenable.errors import *
 
 def test_families(api):
-    api.plugins.families()
+    families = api.plugins.families()
+    assert isinstance(families, list)
+    for f in families:
+        check(f, 'count', int)
+        check(f, 'id', int)
+        check(f, 'name', str)
 
 def test_family_details_family_id_typeerror(api):
     with pytest.raises(TypeError):
         api.plugins.family_details('nope')
 
 def test_family_details(api):
-    resp = api.plugins.family_details(27)
-    assert resp['id'] == 27
+    f = api.plugins.family_details(27)
+    assert isinstance(f, dict)
+    check(f, 'name', str)
+    check(f, 'id', int)
+    check(f, 'plugins', list)
+    for p in f['plugins']:
+        check(p, 'id', int)
+        check(p, 'name', str)
+    assert f['id'] == 27
 
 def test_plugin_details_plugin_id_typerror(api):
     with pytest.raises(TypeError):
         api.plugins.plugin_details('nope')
 
 def test_plugin_details(api):
-    resp = api.plugins.plugin_details(19506)
-    assert resp['id'] == 19506
+    p = api.plugins.plugin_details(19506)
+    assert isinstance(p, dict)
+    check(p, 'attributes', list)
+    for a in p['attributes']:
+        check(a, 'attribute_name', str)
+        check(a, 'attribute_value', str)
+    check(p, 'family_name', str)
+    check(p, 'id', int)
+    check(p, 'name', str)
+    assert p['id'] == 19506
