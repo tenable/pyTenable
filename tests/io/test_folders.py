0bee3fcb03e8098f34323c1d64bb2a633cdef647
Steven McGrath <steve@chigeek.com>
2018-11-01 14:25:47 -0500

    folder outputs tested out #36

--- tests/io/test_folders.py
+++ tests/io/test_folders.py
@@ -21,4 +21,12 @@ def test_edit(api, folder):
     api.folders.edit(folder, str(uuid.uuid4())[:20])
 
 def test_list(api, folder):
-    assert folder in [f['id'] for f in api.folders.list()]
+    folders = api.folders.list()
+    assert isinstance(folders, list)
+    for f in folders:
+        check(f, 'custom', int)
+        check(f, 'default_tag', int)
+        check(f, 'name', str)
+        check(f, 'type', str)
+        check(f, 'id', int)
+        check(f, 'unread_count', int)
