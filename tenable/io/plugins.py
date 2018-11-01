e7d7125e2f36a1084189c1adc8a74b35f4fd9344
Steven McGrath <steve@chigeek.com>
2018-11-01 12:43:24 -0500

    inlined plugins api docs

--- tenable/io/plugins.py
+++ tenable/io/plugins.py
@@ -1,17 +1,41 @@
+'''
+plugins
+=======
+
+The following methods allow for interaction into the Tenable.io 
+`plugins <https://cloud.tenable.com/api#/resources/plugins>`_ API endpoints.
+
+Methods available on ``tio.plugins``:
+
+.. rst-class:: hide-signature
+.. autoclass:: PluginsAPI
+
+    .. automethod:: families
+    .. automethod:: family_details
+    .. automethod:: plugin_details
+'''
 from .base import TIOEndpoint
 
 class PluginsAPI(TIOEndpoint):
     def families(self):
         '''
+        List the available plugin families.
+
         `plugins: families <https://cloud.tenable.com/api#/resources/plugins/families>`_
 
         Returns:
             list: List of plugin familiy resource records.
+
+        Examples:
+            >>> for family in tio.plugins.families():
+            ...     pprint(family)
         '''
         return self._api.get('plugins/families').json()['families']
 
     def family_details(self, id):
         '''
+        Retrieve the details for a specific plugin family.
+
         `plugins: family-details <https://cloud.tenable.com/api#/resources/plugins/family-details>`_
 
         Args:
@@ -21,6 +45,9 @@ class PluginsAPI(TIOEndpoint):
             dict: 
                 Returns a dictionary stating the id, name, and plugins that are
                 housed within the plugin family.
+
+        Examples:
+            >>> family = tio.plugins.family_details(1)
         '''
         return self._api.get('plugins/families/{}'.format(
                 self._check('id', id, int)
@@ -28,6 +55,8 @@ class PluginsAPI(TIOEndpoint):
 
     def plugin_details(self, id):
             '''
+            Retrieve the details for a specific plugin.
+
             `plugins: plugin-details <https://cloud.tenable.com/api#/resources/plugins/plugin-details>`_
 
             Args:
@@ -36,7 +65,11 @@ class PluginsAPI(TIOEndpoint):
             Returns:
                 dict:
                     A dictionary stating the id, name, family, and any other
-                    relevent attributes associated to the plugin.
+                    relevant attributes associated to the plugin.
+
+            Examples:
+                >>> plugin = tio.plugins.plugin_details(19506)
+                >>> pprint(plugin)
             '''
             return self._api.get('plugins/plugin/{}'.format(
                 self._check('id', id, int))).json()
