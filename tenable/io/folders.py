ed57564c6342a75d0a40c7cfa28c51883b10c6e0
Steven McGrath <steve@chigeek.com>
2018-11-01 12:42:49 -0500

    inlined folder api docs #25

--- tenable/io/folders.py
+++ tenable/io/folders.py
@@ -1,8 +1,27 @@
+'''
+folders
+=======
+
+The following methods allow for interaction into the Tenable.io 
+`folders <https://cloud.tenable.com/api#/resources/folders>`_ API endpoints.
+
+Methods available on ``tio.folders``:
+
+.. rst-class:: hide-signature
+.. autoclass:: FoldersAPI
+
+    .. automethod:: create
+    .. automethod:: delete
+    .. automethod:: edit
+    .. automethod:: list
+'''
 from .base import TIOEndpoint
 
 class FoldersAPI(TIOEndpoint):
     def create(self, name):
         '''
+        Create a folder.
+
         `folders: create <https://cloud.tenable.com/api#/resources/folders/create>`_
 
         Args:
@@ -11,6 +30,9 @@ class FoldersAPI(TIOEndpoint):
 
         Returns:
             int: The new folder id.
+
+        Examples:
+            >>> folder = tio.folders.create('New Folder Name')
         '''
         return self._api.post('folders', json={
             'name': self._check('name', name, str)
@@ -18,6 +40,8 @@ class FoldersAPI(TIOEndpoint):
 
     def delete(self, id):
         '''
+        Delete a folder.
+
         `folders: delete <https://cloud.tenable.com/api#/resources/folders/delete>`_
 
         Args:
@@ -25,11 +49,16 @@ class FoldersAPI(TIOEndpoint):
 
         Returns:
             None
+
+        Examples:
+            >>> tio.folders.delete(1)
         '''
         self._api.delete('folders/{}'.format(self._check('id', id, int)))
 
     def edit(self, id, name):
         '''
+        Edit a folder.
+
         `folders: edit <https://cloud.tenable.com/api#/resources/folders/edit>`_
 
         Args:
@@ -38,6 +67,9 @@ class FoldersAPI(TIOEndpoint):
 
         Returns:
             None: The folder was successfully renamed.
+
+        Examples:
+            >>> tio.folders.edit(1, 'Updated Folder Name')
         '''
         self._api.put('folders/{}'.format(self._check('id', id, int)), json={
             'name': self._check('name', name, str)
@@ -45,9 +77,15 @@ class FoldersAPI(TIOEndpoint):
 
     def list(self):
         '''
+        Lists the available folders.
+
         `folders: list <https://cloud.tenable.com/api#/resources/folders/list>`_
 
         Returns:
             list: List of folder resource records.
+
+        Examples:
+            >>> for folder in tio.folders.list():
+            ...     pprint(folder)
         '''
         return self._api.get('folders').json()['folders']
