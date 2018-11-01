6c14c5431b77011cc65b33cbd19a771f853c7da6
Steven McGrath <steve@chigeek.com>
2018-11-01 14:20:11 -0500

    filters outputs tested #37

--- tests/io/test_filters.py
+++ tests/io/test_filters.py
@@ -1,18 +1,36 @@
 from .fixtures import *
 from tenable.errors import *
 
+from pprint import pprint
+
 def test_agent_filters(api):
     filters = api.filters.agents_filters()
     assert isinstance(filters, dict)
+    for f in filters:
+        check(filters[f], 'choices', list, allow_none=True)
+        check(filters[f], 'operators', list)
+        check(filters[f], 'pattern', str, allow_none=True)
 
 def test_workbench_vuln_filters(api):
     filters = api.filters.workbench_vuln_filters()
     assert isinstance(filters, dict)
+    for f in filters:
+        check(filters[f], 'choices', list, allow_none=True)
+        check(filters[f], 'operators', list)
+        check(filters[f], 'pattern', str, allow_none=True)
 
 def test_workbench_asset_filters(api):
     filters = api.filters.workbench_asset_filters()
     assert isinstance(filters, dict)
+    for f in filters:
+        check(filters[f], 'choices', list, allow_none=True)
+        check(filters[f], 'operators', list)
+        check(filters[f], 'pattern', str, allow_none=True)
 
 def test_scan_filters(api):
     filters = api.filters.scan_filters()
     assert isinstance(filters, dict)
+    for f in filters:
+        check(filters[f], 'choices', list, allow_none=True)
+        check(filters[f], 'operators', list)
+        check(filters[f], 'pattern', str, allow_none=True)
