e59e43d72ce62e57e7a2af39e199677b6e9ccc52
Steven McGrath <steve@chigeek.com>
2018-11-29 16:10:36 -0600

    added dateutil package to mocking [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-autodoc_mock_imports = ['lxml', 'dateutil.parser']
+autodoc_mock_imports = ['lxml', 'dateutil', 'dateutil.parser']
 
 import os, sys, datetime
 sys.path.insert(0, os.path.abspath('..'))
