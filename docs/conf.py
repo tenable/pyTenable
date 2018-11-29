623689df55aad1459b0031b3e19f5ba9fd089fe1
Steven McGrath <steve@chigeek.com>
2018-11-29 15:57:48 -0600

    added mock import for lxml [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+autodoc_mock_imports = ['lxml']
+
 import os, sys, datetime
 sys.path.insert(0, os.path.abspath('..'))
 
