45d6349b18cddf3daa932317358b2733d15097cb
Steven McGrath <steve@chigeek.com>
2019-01-14 10:13:39 -0600

    Adding testrunner config

---
+++ testing.settings.sh
@@ -0,0 +1,29 @@
+export TESTING_PYTHON_VERSIONS="2.7.15 3.4.9 3.5.6 3.6.7 3.7.1"
+
+export TIO_TEST_ADMIN_ACCESS=""
+export TIO_TEST_ADMIN_SECRET=""
+export TIO_TEST_STD_ACCESS=""
+export TIO_TEST_STD_SECRET=""
+export NESSUS_LICENSE=""
+export TIO_REG_ACCESS_KEY=""
+export TIO_REG_SECRET_KEY=""
+export SC_TEST_HOST="securitycenter.home.cugnet.net"
+export SC_TEST_PORT=443
+export SC_TEST_USER=""
+export SC_TEST_PASS=""
+
+function perform_cleanup {
+    echo "Cleaning up the Nessus Reports."
+    rm -f *.nessus
+}
+
+function run_tests {
+    if [ "${1}" == "record" ];then
+        pytest --cov=tenable tests
+    elif [ "${1}" == "integration" ];then
+        pytest --disable-vcr --cov=tenable tests
+    else
+        pytest --vcr-record=none --cov=tenable tests
+    fi
+    return $?
+}
