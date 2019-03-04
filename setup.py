184240efee8de040cc1eca1645e72fc8ca515206
Steven McGrath <steve@chigeek.com>
2019-03-04 14:37:32 -0600

    updated travis

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
 
 setup(
     name='pyTenable',
-    version='0.3.10',
+    version='0.3.11',
     description='Python library to interface into Tenable\'s products and applications',
     author='Tenable, Inc.',
     long_description=long_description,
