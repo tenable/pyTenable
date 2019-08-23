755ed4d890fe5828291c225ad365c3747debb1f7
Steve McGrath <steve@chigeek.com>
2019-08-23 14:30:43 -0500

    Added graftable plugin family details.

--- tenable/io/plugins.py
+++ tenable/io/plugins.py
@@ -37,8 +37,55 @@ class PluginIterator(TIOIterator):
         page_count (int): The number of record returned from the current page.
         total (int):
             The total number of records that exist for the current request.
+        populate_maptable (bool):
+            Informs the iterator whether to construct the plugin to family maps
+            for injecting the plugin family data into each item.
     '''
-    pass
+    _maptable = None
+    populate_maptable = False
+
+    def _populate_family_cache(self):
+        '''
+        Generates the maptable to use to graft on the plugin family information
+        to the plugins.  Effectively what we doing is generating a dictionary of
+        2 subdictionaries.  Each one of these is a simple hash table allowing
+        the iterator to resolve the name of the family by ID and the family ID
+        by the plugin membership.  This information is currently lacking in the
+        plugin listing output and was requested by a customer.
+
+        .. note::
+            This currently seems to add about 7-10 seconds before the first item
+            is returned, as it seems to take this long to generate the data. We
+            can focus on reducing this time later on with the introduction of
+            multi-threaded iterators && async API calls.
+        '''
+        self._maptable = {
+            'plugins': dict(),
+            'families': dict()
+        }
+        for family in self._api.plugins.families():
+            self._maptable['families'][family['id']] = family['name']
+
+        for fam_id in self._maptable['families'].keys():
+            for plugin in self._api.plugins.family_details(fam_id)['plugins']:
+                self._maptable['plugins'][plugin['id']] = fam_id
+
+    def next(self):
+        item = super(PluginIterator, self).next()
+
+        # If the populate_maptable flag is set, then we will build the mappings.
+        if not self._maptable and self.populate_maptable:
+            self._populate_family_cache()
+
+        # If the maptable exists, then graft on the plugin family information
+        # on to to the item.
+        if self._maptable:
+            fid = self._maptable['plugins'][item['id']]
+            item['family_id'] = fid
+            item['family_name'] = self._maptable['families'][fid]
+
+        return item
+
 
 
 class PluginsAPI(TIOEndpoint):
@@ -129,6 +176,14 @@ class PluginsAPI(TIOEndpoint):
             Retrieving all of the plugins updated since 2019-01-01:
 
             >>> for plugin in tio.plugins.list(last_updated=date(2019, 1, 1)):
+            ...     pprint(plugin)
+
+            Informing the iterator to cache the plugin family data for injection
+            into each item:
+
+            >>> plugins = tio.plugins.list(last_updated=date(2019, 1, 1))
+            >>> plugins.populate_maptable = True
+            >>> for plugin in plugins:
             ...     pprint(plugin)
         '''
         return PluginIterator(self._api,
