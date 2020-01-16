2ad4eec2f3c361e60368dc7a41997c85fd311ee5
Steve McGrath <steve@chigeek.com>
2020-01-15 18:03:05 -0600

    Added additional exception handling for retries.

--- tenable/base.py
+++ tenable/base.py
@@ -2,6 +2,7 @@
 .. autoclass:: APIResultsIterator
 '''
 import requests, sys, platform, logging, re, time, logging, warnings, json
+from requests.exceptions import ConnectionError as RequestsConnectionError
 from .errors import *
 from . import __version__, __author__
 
@@ -307,6 +308,7 @@ class APISession(object):
             An optional identifier for the application to discern it amongst
             other API calls.
     '''
+    _log_precog = False
     _restricted_paths = dict()
     _vendor = 'unknown'
     _product = 'unknown'
@@ -339,7 +341,7 @@ class APISession(object):
 
     def __init__(self, url=None, retries=None, backoff=None,
                  ua_identity=None, session=None, proxies=None,
-                 vendor=None, product=None, build=None):
+                 vendor=None, product=None, build=None, prelog=False):
         if url:
             self._url = url
         if retries and isinstance(retries, int):
@@ -356,6 +358,8 @@ class APISession(object):
             self._product = product
         if build:
             self._build = build
+        if prelog:
+            self._log_precog = True
         self._log = logging.getLogger('{}.{}'.format(
             self.__module__, self.__class__.__name__))
         self._build_session(session)
@@ -435,12 +439,13 @@ class APISession(object):
 
         while retries <= self._retries:
             if (('params' in kwargs and kwargs['params'])
-              or ('json' in kwargs and kwargs['json'])):
+              or ('json' in kwargs and kwargs['json'])
+              or self._log_precog):
                 if path not in self._restricted_paths:
                     # If the path is not one of the paths that would contain
                     # sensitive data (such as login information) then pass the
                     # log on unredacted.
-                    self._log.debug('path={}, query={}, body={}'.format(
+                    self._log.debug('{} {}, query={}, body={}'.format(method,
                         path, kwargs.get('params', {}), kwargs.get('json', {})))
                 else:
                     # The path was a restricted path, generate the log, however
@@ -449,47 +454,54 @@ class APISession(object):
                         path, 'REDACTED', 'REDACTED'))
 
             # Make the call to the API and pull the status code.
-            resp = self._session.request(method,
-                '{}/{}'.format(self._url, path), **kwargs)
-            status = resp.status_code
-
-            # If there is a Request UUID then we will want to log the UUID just
-            # incase we may need it for tracking down what happened within the
-            if resp.headers.get('x-request-uuid'):
-                self._log.debug('Request-UUID {} for {}'.format(
-                    resp.headers.get('x-request-uuid'),
-                    '{}/{}'.format(self._url, path)))
-
-            if status in retry_codes:
-                # Under the following return codes, we will want to attempt to
-                # retry our call.  If we see the "Retry-After" header, then we
-                # will respect that.  If no "Retry-After" header exists, then
-                # we will use the _backoff attribute to build a back-off timer
-                # based on the number of retries we have already performed.
+            try:
+                resp = self._session.request(method,
+                    '{}/{}'.format(self._url, path), **kwargs)
+                status = resp.status_code
+            except RequestsConnectionError as err:
+                self._log.error('Connection Reset {}'.format(str(err)))
+                time.sleep(0.1)
                 retries += 1
-                time.sleep(float(resp.headers.get(
-                    'retry-after', float(retries) * float(self._backoff))))
-
-                # The need to potentially modify the request for subsequent
-                # calls if the whole reason that we aren't using the default
-                # Retry logic that urllib3 supports.
-                kwargs = self._retry_request(resp, retries, kwargs)
-                continue
-
-            elif status in self._error_codes.keys():
-                # If a status code that we know about has returned, then we will
-                # want to raise the appropriate Error.
-                raise self._error_codes[status](resp)
-
-            elif status >= 200 and status <= 299:
-                # As everything looks ok, lets pass the response on to the error
-                # checker and then return the response.
-                return self._resp_error_check(resp, **kwargs)
-
             else:
-                # If all else fails, raise an error stating that we don't even
-                # know whats happening.
-                raise UnknownError(resp)
+                # If there is a Request UUID then we will want to log the UUID
+                # just incase we may need it for tracking down what happened
+                # within the
+                if resp.headers.get('x-request-uuid'):
+                    self._log.debug('Request-UUID {} for {}'.format(
+                        resp.headers.get('x-request-uuid'),
+                        '{}/{}'.format(self._url, path)))
+
+                if status in retry_codes:
+                    # Under the following return codes, we will want to attempt
+                    # to retry our call.  If we see the "Retry-After" header,
+                    # then we will respect that.  If no "Retry-After" header
+                    # exists, then we will use the _backoff attribute to build
+                    # a back-off timer based on the number of retries we have
+                    # already performed.
+                    retries += 1
+                    time.sleep(float(resp.headers.get(
+                        'retry-after', float(retries) * float(self._backoff))))
+
+                    # The need to potentially modify the request for subsequent
+                    # calls if the whole reason that we aren't using the default
+                    # Retry logic that urllib3 supports.
+                    kwargs = self._retry_request(resp, retries, kwargs)
+                    continue
+
+                elif status in self._error_codes.keys():
+                    # If a status code that we know about has returned, then we
+                    # will want to raise the appropriate Error.
+                    raise self._error_codes[status](resp)
+
+                elif status >= 200 and status <= 299:
+                    # As everything looks ok, lets pass the response on to the
+                    # error checker and then return the response.
+                    return self._resp_error_check(resp, **kwargs)
+
+                else:
+                    # If all else fails, raise an error stating that we don't
+                    # even know whats happening.
+                    raise UnknownError(resp)
         raise RetryError(resp, retries)
 
     def get(self, path, **kwargs):
--- tenable/sc/__init__.py
+++ tenable/sc/__init__.py
@@ -183,7 +183,7 @@ class TenableSC(APISession):
                  password=None, port=443, ssl_verify=False, cert=None,
                  adapter=None, scheme='https', retries=None, backoff=None,
                  ua_identity=None, session=None, proxies=None,
-                 vendor=None, product=None, build=None):
+                 vendor=None, product=None, build=None, prelog=False):
         # As we will always be passing a URL to the APISession class, we will
         # want to construct a URL that APISession (and further requests)
         # understands.
@@ -204,7 +204,8 @@ class TenableSC(APISession):
             proxies=proxies,
             vendor=vendor,
             product=product,
-            build=build)
+            build=build,
+            prelog=prelog)
 
         # If a client-side certificate is specified, then we will want to add
         # it into the session object as well.  The cert parameter is expecting
