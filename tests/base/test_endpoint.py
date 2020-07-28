56addb4395b1cafd93976026e72edebc9eb873b2
Steve McGrath <steve@chigeek.com>
2020-07-28 11:39:56 -0500

    Testing suite for new base classes

---
+++ tests/base/test_endpoint.py
@@ -0,0 +1,10 @@
+from tenable.base.endpoint import APIEndpoint
+import pytest
+
+
+def test_apiendpoint():
+    '''
+    Yes this is always going to return true, but there isnt anything to test
+    in the module yet.
+    '''
+    assert APIEndpoint == APIEndpoint
