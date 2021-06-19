f672b25844d8a356b7616335d25831e3d40d5ddc
mvalliammal <84954283+mvalliammal@users.noreply.github.com>
2021-06-19 09:52:59 +0530

    Update test_remediation_scans.py

--- tests/io/test_remediation_scans.py
+++ tests/io/test_remediation_scans.py
@@ -11,7 +11,7 @@ from tests.checker import check, single
 
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_create.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_create.yaml', record_mode='all')
 def remedy_scan(request, api):
     '''
     test to create remediation scan document advanced template
@@ -35,7 +35,7 @@ def remedy_scan(request, api):
 
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_list.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_list.yaml', record_mode='all')
 def remedy_scanned_list(api):
     '''
     test to get remediation scan list
@@ -47,7 +47,7 @@ def remedy_scanned_list(api):
 
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_create_scan_document_template_typeerror.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_create_scan_document_template_typeerror.yaml', record_mode='all')
 def test_remedyscan_create_scan_document_template_typeerror(api):
     '''
     test to raise exception when type of template param does not match the expected type.
@@ -56,7 +56,7 @@ def test_remedyscan_create_scan_document_template_typeerror(api):
         getattr(api.remediationscans, 'create_remediation_scan')({'template': 'advanced'})
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_create_scan_document_template_unexpect_err.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_create_scan_document_template_unexpect_err.yaml', record_mode='all')
 def test_remedyscan_create_scan_document_template_unexpect_value_err(api):
     '''
     test to raise exception when template param value does not match the choices.
@@ -65,7 +65,7 @@ def test_remedyscan_create_scan_document_template_unexpect_value_err(api):
         getattr(api.remediationscans, '_create_scan_document')({'template': 'nothing_here'})
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_create_scan_document_template.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_create_scan_document_template.yaml', record_mode='all')
 def test_remedyscan_create_scan_socument_template_pass(api):
     '''
     test to create scan document basic template
@@ -78,7 +78,7 @@ def test_remedyscan_create_scan_socument_template_pass(api):
 
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_create_policies_id.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_create_policies_id.yaml', record_mode='all')
 def test_remedyscan_create_scan_document_policies_id_pass(api):
     '''
     test to create scan document policy param using id
@@ -92,7 +92,7 @@ def test_remedyscan_create_scan_document_policies_id_pass(api):
     assert resp['settings']['policy_id'] == policy['id']
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_create_policies.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_create_policies.yaml', record_mode='all')
 def test_remedyscan_create_scan_document_policies_name_pass(api):
     '''
     test to create scan document with policy param using name
@@ -109,7 +109,7 @@ def test_remedyscan_create_scan_document_policies_name_pass(api):
 #def test_remedyscan_create_scan_document_targets
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_scan_doc_unexpect_err.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_scan_doc_unexpect_err.yaml', record_mode='all')
 def test_remedyscan_create_scan_document_scanner_unexpect_err(api):
     '''
     test to raise exception when scanner param value does not match the choices.
@@ -118,7 +118,7 @@ def test_remedyscan_create_scan_document_scanner_unexpect_err(api):
         getattr(api.remediationscans, '_create_scan_document')({'scanner': 'nothing to see here'})
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_scanner_uuid.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_scanner_uuid.yaml', record_mode='all')
 def test_remedyscan_create_scan_document_scanner_uuid_pass(api):
     '''
     test to create scan document with scanner uuid param
@@ -132,7 +132,7 @@ def test_remedyscan_create_scan_document_scanner_uuid_pass(api):
     assert resp['settings']['scanner_id'] == scanner['id']
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_scanner_name.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_scanner_name.yaml', record_mode='all')
 def test_remedyscan_create_scan_document_scanner_name_pass(api):
     '''
     test to create scan document with scanner name param
@@ -146,7 +146,7 @@ def test_remedyscan_create_scan_document_scanner_name_pass(api):
     assert resp['settings']['scanner_id'] == scanner['id']
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_notemplate_pass.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_notemplate_pass.yaml', record_mode='all')
 def test_remedyscan_create_no_template_pass(scan):
     '''
     test to create scan when no template is provided by user
@@ -174,7 +174,7 @@ def test_remedyscan_create_no_template_pass(scan):
     check(scan, 'uuid', str)
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_status.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_status.yaml', record_mode='all')
 def test_remedyscan_status(api):
     '''
     test to check scan status
@@ -191,7 +191,7 @@ def test_remedyscan_status(api):
     single(status, str)
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_timezones.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_timezones.yaml', record_mode='all')
 def test_remedyscan_timezones(api):
     '''
     test to get list of allowed timezone strings
@@ -199,7 +199,7 @@ def test_remedyscan_timezones(api):
     assert isinstance(api.scans.timezones(), list)
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_targets_success.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_targets_success.yaml', record_mode='all')
 def test_remedyscan_check_auto_targets_success(api):
     '''
     test to evaluates a list of targets and/or tags against
@@ -213,7 +213,7 @@ def test_remedyscan_check_auto_targets_success(api):
     check(resp, 'total_missed_targets', int)
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_targets_limit_type_err.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_targets_limit_type_err.yaml', record_mode='all')
 def test_remedyscan_check_auto_targets_limit_typeerror(api):
     '''
     test to raise exception when type of limit param does not match the expected type.
@@ -222,7 +222,7 @@ def test_remedyscan_check_auto_targets_limit_typeerror(api):
         api.scans.check_auto_targets('nope', 5, targets=['192.168.16.108'])
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_targets_matched_resource_err.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_targets_matched_resource_err.yaml', record_mode='all')
 def test_remedyscan_check_auto_targets_matched_resource_limit_typeerror(api):
     '''
     test to raise exception when type of matched_resource_limit param
@@ -232,7 +232,7 @@ def test_remedyscan_check_auto_targets_matched_resource_limit_typeerror(api):
         api.scans.check_auto_targets(10, 'nope', targets=['127.0.0.1'])
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_targets_networkuuid_err.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_targets_networkuuid_err.yaml', record_mode='all')
 def test_remedyscan_check_auto_targets_network_uuid_unexpectedvalueerror(api):
     '''
     test to raise exception when network_uuid param value does not match the choices.
@@ -241,7 +241,7 @@ def test_remedyscan_check_auto_targets_network_uuid_unexpectedvalueerror(api):
         api.scans.check_auto_targets(10, 5, network_uuid='nope', targets=['127.0.0.1'])
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_targets_networktype_err.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_targets_networktype_err.yaml', record_mode='all')
 def test_remedyscan_check_auto_targets_network_uuid_typeerror(api):
     '''
     test to raise exception when type of network_uuid param does not match the expected type.
@@ -250,7 +250,7 @@ def test_remedyscan_check_auto_targets_network_uuid_typeerror(api):
         api.scans.check_auto_targets(10, 5, network_uuid=1, targets=['127.0.0.1'])
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_targets_tags_unexpect_err.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_targets_tags_unexpect_err.yaml', record_mode='all')
 def test_remedyscan_check_auto_targets_tags_unexpectedvalueerror(api):
     '''
     test to raise exception when type of any value in tags param
@@ -260,7 +260,7 @@ def test_remedyscan_check_auto_targets_tags_unexpectedvalueerror(api):
         api.scans.check_auto_targets(10, 5, tags=['nope'], targets=['127.0.0.1'])
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_targets_tags.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_targets_tags.yaml', record_mode='all')
 def test_remedyscan_check_auto_targets_tags_typeerror(api):
     '''
     test to raise exception when type of tags param does not match the expected type.
@@ -269,7 +269,7 @@ def test_remedyscan_check_auto_targets_tags_typeerror(api):
         api.scans.check_auto_targets(10, 5, tags=1, targets=['127.0.0.1'])
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_targets_type.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_targets_type.yaml', record_mode='all')
 def test_remedyscan_check_auto_targets_typeerror(api):
     '''
     test to raise exception when type of targets param does not match the expected type.
@@ -278,7 +278,7 @@ def test_remedyscan_check_auto_targets_typeerror(api):
         api.scans.check_auto_targets(10, 5, targets=1)
 
 @pytest.mark.vcr()
-@vcr.use_cassette('cassettes/test_remedyscan_results_datatype.yaml', record_mode='once')
+@vcr.use_cassette('cassettes/test_remedyscan_results_datatype.yaml', record_mode='all')
 def test_remedyscan_results_datatype(api):
     '''
     test remedy scan results
