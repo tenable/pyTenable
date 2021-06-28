a2743d9070e8d8d0c9f897dd90a364bdc87a44b6
Dheeraj RP <dheerajrp66@gmail.com>
2021-06-28 21:54:27 +0530

    In-387: Added tests for missing imports

--- tests/cs/test_compile.py
+++ tests/cs/test_compile.py
@@ -1,14 +1,12 @@
 '''
-test comppile
+test compile
 '''
-import os
 import pytest
-import py_compile
 from tenable.errors import UnexpectedValueError
 from tenable.cs import ContainerSecurity
 
 
-def test_init_catch_name_error():
+def test_cs_init_catch_name_error():
     '''
     test to raise the exception when no api keys are provided
     '''
@@ -20,27 +18,3 @@ def test_init_catch_name_error():
         assert True
     except UnexpectedValueError:
         pytest.raises(UnexpectedValueError)
-
-
-def test_compile_success():
-    '''
-    test to compile the init file
-    '''
-    with open('container.cs', 'a') as fobj:
-        fobj.write('import os')
-        os.chdir('..')
-        os.chdir('..')
-        file = open('tenable/cs/__init__.py', 'r')
-        fobj.write('\n')
-        fobj.writelines(file)
-        file.close()
-    fobj.close()
-    try:
-        py_compile.compile('tests/cs/container.cs')
-    except py_compile.PyCompileError as err:
-        print(err.msg)
-
-    os.remove('tests/cs/container.cs')
-    if os.path.exists('tests/cs/__pycache__/container.cpython-38.pyc'):
-        os.remove('tests/cs/__pycache__/container.cpython-38.pyc')
-
