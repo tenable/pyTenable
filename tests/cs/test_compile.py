21be5053e13a3970d032d7c2effa50dc09dffe75
Dheeraj RP <dheerajrp66@gmail.com>
2021-06-29 15:06:20 +0530

    IN-387: Modified tests, addressed marking issue

--- tests/cs/test_compile.py
+++ tests/cs/test_compile.py
@@ -2,8 +2,16 @@
 test compile
 '''
 import pytest
-from tenable.errors import UnexpectedValueError
+
 from tenable.cs import ContainerSecurity
+from tenable.cs.base import CSIterator, CSEndpoint
+from tenable.cs.images import ImageIterator, ImageAPI
+from tenable.cs.reports import ReportAPI
+from tenable.cs.repositories import RepositoryAPI, RepositoryIterator
+from tenable.cs.uploads import UploadAPI
+from tenable.cs.usage import UsageAPI
+from tenable.errors import UnexpectedValueError
+from tests.cs.conftest import api
 
 
 def test_cs_init_catch_name_error():
@@ -12,9 +20,21 @@ def test_cs_init_catch_name_error():
     '''
     try:
         ContainerSecurity()
+        CSEndpoint(api)
+        CSIterator(api)
+        ImageIterator(api)
+        ImageAPI(api)
+        ReportAPI(api)
+        RepositoryIterator(api)
+        RepositoryAPI(api)
+        UploadAPI(api)
+        UsageAPI(api)
+
     except NameError as error:
         print('\n', 'The following error exists-\n', error)
         pytest.raises(NameError)
         assert True
-    except UnexpectedValueError:
+    except UnexpectedValueError as error:
+        print('\n', 'The following error exists-\n', error)
         pytest.raises(UnexpectedValueError)
+        assert True
