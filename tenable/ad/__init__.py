842ed03ea3012f25247807a0fc083f817891b04c
shcoderepo <shbiradar@yahoo.com>
2021-08-09 15:38:55 +0530

    Models and api folder removed.  Main programs added

--- tenable/ad/__init__.py
+++ tenable/ad/__init__.py
@@ -1,18 +1,33 @@
-# coding: utf-8
-
-# flake8: noqa
-
-"""
-    Tenable.ad - Client API
-
-    API to interact with Tenable.ad   
-
-    OpenAPI spec version: production
-
-"""
-
 from __future__ import absolute_import
 
-# import apis into sdk package
-# import ApiClient
-# import models into sdk package
+from tenable.ad.about import AboutApi
+# import apis into api package
+from tenable.ad.ad_object import ADObjectApi
+from tenable.ad.alert import AlertApi
+from tenable.ad.api_key import APIKeyApi
+from tenable.ad.application_setting import ApplicationSettingApi
+from tenable.ad.attack_alert import AttackAlertApi
+from tenable.ad.attack import AttackApi
+from tenable.ad.category import CategoryApi
+from tenable.ad.checker import CheckerApi
+from tenable.ad.checker_option import CheckerOptionApi
+from tenable.ad.dashboard import DashboardApi
+from tenable.ad.deviance import DevianceApi
+from tenable.ad.directory import DirectoryApi
+from tenable.ad.email_notifier import EmailNotifierApi
+from tenable.ad.event import EventApi
+from tenable.ad.infrastructure import InfrastructureApi
+from tenable.ad.ldap_configuration import LDAPConfigurationApi
+from tenable.ad.license import LicenseApi
+from tenable.ad.preference import PreferenceApi
+from tenable.ad.profile import ProfileApi
+from tenable.ad.reason import ReasonApi
+from tenable.ad.role import RoleApi
+from tenable.ad.saml_configuration import SAMLConfigurationApi
+from tenable.ad.score import ScoreApi
+from tenable.ad.syslog import SyslogApi
+from tenable.ad.topology import TopologyApi
+from tenable.ad.user import UserApi
+from tenable.ad.widget import WidgetApi
+
+# flake8: noqa
