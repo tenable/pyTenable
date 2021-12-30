0f1e92f6afd1444eb2a05f539c4fe63915c3ef0f
Harsh Shah <hashah.ctr@tenable.com>
2021-12-30 22:56:00 +0530

    PR review comments

--- tenable/io/v3/mssp/logos/api.py
+++ tenable/io/v3/mssp/logos/api.py
@@ -29,10 +29,10 @@ class LogosAPI(ExploreBaseEndpoint):
 
     def add(self, logo: Union[BytesIO, BinaryIO], name: str) -> UUID:
         '''
-        Adds a new logo
+        Adds a logo to the Tenable.io MSSP Portal
 
         Args:
-            fobj (FileObject, optional):
+            fobj (FileObject):
                 A file-like object of the logo.
             name (str):
                 Name of the logo.
@@ -52,8 +52,6 @@ class LogosAPI(ExploreBaseEndpoint):
         '''
         Returns list of logos
 
-        Args:
-
         Returns:
             :obj:`list`:
                 Returns all the logo details as a list.
@@ -65,34 +63,81 @@ class LogosAPI(ExploreBaseEndpoint):
 
     def search(self):
         '''
-        Get the listing of Logos from MSSP portal.
+        Initiate a search
 
         Args:
-            *filters (tuple, optional):
-                Filters are tuples in the form of
-                ('NAME', 'OPERATOR', 'VALUE'). Multiple filters can be used
-                to filter down the data being returned from the API.
-                Examples:
-                    - ``('name', 'eq', 'example')``
-            limit (int, optional):
-                The number of records to retrieve.  Default is 50
-            sort (tuple, optional):
-                A tuple of tuples identifying the the field and sort order of
-                the field.
+            resource (str):
+                The json key to fetch the data from response
+            api_path (str):
+                API path for search endpoint
+            is_sort_with_prop (bool):
+                If set to True sort structure will be in form of
+                {'property':'field_name','order': 'asc'} else
+                {'field_name': 'asc'}
             fields (list):
-                list of fields to retrieve.
+                The list of field names to return.
+                Example:
+                    - ``['field1', 'field2']``
+            sort (list(tuple)):
+                A list of dictionaries describing how to sort the data
+                that is to be returned.
+                Examples:
+                    - ``[{'last_observed': 'desc'}]``
+            filter (tuple, dict):
+                A nestable filter object detailing how to filter the results
+                down to the desired subset.
+                Examples:
+                    >>> ('or', ('and', ('test', 'oper', '1'),
+                                   ('test', 'oper', '2')
+                            ),
+                    'and', ('test', 'oper', 3)
+                   )
+                    >>> {'or': [
+                    {'and': [
+                        {'value': '1', 'operator': 'oper', 'property': '1'},
+                        {'value': '2', 'operator': 'oper', 'property': '2'}
+                        ]
+                    }],
+                    'and': [
+                        {'value': '3', 'operator': 'oper', 'property': 3}
+                        ]
+                    }
+                As the filters may change and sortable fields may change over
+                time, it's highly recommended that you look at the output of
+                the :py:meth:`tio.v3.vm.filters.asset_filters()`
+                endpoint to get more details.
+            limit (int):
+                How many objects should be returned in each request.
+                 Default is 1000.
+            next (str):
+                The pagination token to use when requesting the next page of
+                results.  This token is presented in the previous response.
+            return_resp (bool):
+                If set to true, will override the default behavior to return
+                an iterable and will instead return the results for the
+                specific page of data.
+            return_csv (bool):
+                If set to true, It wil return the CSV Iteratble
+            iterator_cls:
+                If specified, will override the default iterator class that
+                will be used for instantiating the iterator.
+            schema_cls:
+                If specified, will override the default schema class that
+                will be used to validate the
+
         Returns:
-            :obj:`LogosIterator`:
-                An iterator that handles the page management of the requested
-                records.
-        Examples:
-            Todo
+            Iterable:
+                The iterable that handles the pagination and potentially
+                async requests for the job.
+            requests.Response:
+                If ``return_json`` was set to ``True``, then a response
+                object is instead returned instead of an iterable.
         '''
         raise NotImplementedError('Search method will be implemented later')
 
     def details(self, logo_id: UUID) -> Dict:
         '''
-        Returns Logo details
+        Returns details for the specified logo in the Tenable.io MSSP Portal.
 
         Args:
             logo_id (UUID):
@@ -109,11 +154,17 @@ class LogosAPI(ExploreBaseEndpoint):
 
     def update(self, logo_id: UUID, **kw) -> str:
         '''
-        Returns Logo details
+        Updates a logo in the Tenable.io MSSP Portal.
+        This update overwrites the existing logo.
 
         Args:
             logo_id (UUID):
                 The UUID of the logo
+            fobj (FileObject):
+                A file-like object of the logo.
+            name (str):
+                Name of the logo.
+
         Returns:
             :obj:`str`:
                 Return the logo_id.
@@ -176,11 +227,10 @@ class LogosAPI(ExploreBaseEndpoint):
         ))
         self._api.put('api/v3/mssp/accounts/logos', json=payload)
 
-    def download_png(
-        self,
-        logo_id: UUID,
-        fobj: Optional[Union[BytesIO, BinaryIO]] = None
-    ) -> Union[BytesIO, BinaryIO]:
+    def download_png(self,
+                     logo_id: UUID,
+                     fobj: Optional[Union[BytesIO, BinaryIO]] = None
+                     ) -> Union[BytesIO, BinaryIO]:
         '''
         Downloads the logo in either base64 or png format.
 
