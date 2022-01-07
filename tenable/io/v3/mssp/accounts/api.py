f23e226d829ebd784b9c2f9033b4b62e7f469bbd
parvdesai-tenable <padesai.ctr@tenable.com>
2022-01-07 15:50:44 +0530

    search

--- tenable/io/v3/mssp/accounts/api.py
+++ tenable/io/v3/mssp/accounts/api.py
@@ -15,6 +15,8 @@ Methods available on ``tio.v3.mssp.accounts``:
 from typing import List
 
 from tenable.io.v3.base.endpoints.explore import ExploreBaseEndpoint
+from tenable.io.v3.base.iterators.explore_iterator import (CSVChunkIterator,
+                                                           SearchIterator)
 
 
 class AccountsAPI(ExploreBaseEndpoint):
@@ -22,7 +24,83 @@ class AccountsAPI(ExploreBaseEndpoint):
     _conv_json = True
 
     def search(self, **kwargs) -> List:
-        raise NotImplementedError(
-            'This method will be updated once ExploreSearchIterator is \
-                implemented for v3'
-        )
+        '''
+        Retrieves the assets.
+
+        Args:
+            fields (list):
+                The list of field names to return from the Tenable API.
+
+                Example:
+                    - ``['field1', 'field2']``
+            filter (tuple, Dict):
+                A nestable filter object detailing how to filter the results
+                down to the desired subset.
+
+                Examples:
+                    >>> ('or', ('and', ('test', 'oper', '1'),
+                                   ('test', 'oper', '2')
+                            ),
+                    'and', ('test', 'oper', 3)
+                   )
+                    >>> {'or': [
+                    {'and': [
+                        {'value': '1', 'operator': 'oper', 'property': '1'},
+                        {'value': '2', 'operator': 'oper', 'property': '2'}
+                        ]
+                    }],
+                    'and': [
+                        {'value': '3', 'operator': 'oper', 'property': 3}
+                        ]
+                    }
+
+                As the filters may change and sortable fields may change over
+                time, it's highly recommended that you look at the output of
+                the :py:meth:`tio.v3.vm.filters.asset_filters()`
+                endpoint to get more details.
+            sort list(tuple, Dict):
+                A list of dictionaries describing how to sort the data
+                that is to be returned.
+
+                Examples:
+                    - ``[('field_name_1', 'asc'),
+                             ('field_name_2', 'desc')]``
+                    - ``[{'property': 'last_observed', 'order': 'desc'}]``
+            limit (int):
+                Number of objects to be returned in each request.
+                Default is 1000.
+            next (str):
+                The pagination token to use when requesting the next page of
+                results.  This token is presented in the previous response.
+            return_resp (bool):
+                If set to true, will override the default behavior to return
+                an iterable and will instead return the results for the
+                specific page of data.
+            return_csv (bool):
+                If set to true, It wil return the CSV Iterable. Returns all
+                data in text/csv format on each next call with row headers
+                on each page.
+
+        Returns:
+            Returns:
+                Iterable:
+                    The iterable that handles the pagination and potentially
+                    async requests for the job.
+                requests.Response:
+                    If ``return_json`` was set to ``True``, then a response
+                    object is instead returned instead of an iterable.
+
+        Examples:
+            >>> tio.v3.mssp.accounts.search(filter=('netbios_name', 'eq',
+            ...  'SCCM'), fields=['name', 'netbios_name', 'last_login'],
+            ...    limit=2, sort=[('last_observed', 'asc')])
+        '''
+        iclass = SearchIterator
+        if kwargs.get('return_csv', False):
+            iclass = CSVChunkIterator
+        return super().search(resource='accounts',
+                              iterator_cls=iclass,
+                              is_sort_with_prop=False,
+                              api_path=f'{self._path}/search',
+                              **kwargs
+                              )
--- tests/io/v3/mssp/accounts/test_accounts_api.py
+++ tests/io/v3/mssp/accounts/test_accounts_api.py
@@ -1,5 +1,38 @@
-import pytest
 import responses
+
+MSSP_ACCOUNTS_URL = 'https://cloud.tenable.com/api/v3/mssp/accounts'
+
+accounts_us_east = [
+    {
+        'id': '2fd20a81-5dbf-4749-9173-62ba398c0641',
+        'container_name': 'MSSP Example 1',
+        'region': 'US East',
+        'licensed_assets': 0,
+        'licensed_assets_limit': 1024,
+        'licensed_apps': [],
+        'logo_uuid': '46cf08ec-8d67-4232-848e-b229faa86e7a'
+    },
+    {
+        'id': '10a3ad73-6220-4197-97e8-c7acb2a4d19a',
+        'container_name': 'MSSP Example 3',
+        'region': 'US East',
+        'licensed_assets': 0,
+        'licensed_assets_limit': -1,
+        'licensed_apps': [],
+        'notes': 'Notes test',
+        'logo_uuid': '46519267-81d0-4fb6-89dc-39fe8280ee79'
+    },
+    {
+        'id': '9e3f9150-97ef-4a9a-9390-49b5f67423c0',
+        'container_name': 'MSSP Example 5',
+        'region': 'US East',
+        'licensed_assets': 0,
+        'licensed_assets_limit': -1,
+        'licensed_apps': [],
+        'notes': 'These are notes.',
+        'logo_uuid': '5cc5d763-52a1-4943-a3b4-9bca84e9ae27'
+    },
+]
 
 
 @responses.activate
@@ -7,5 +40,25 @@ def test_search(api):
     '''
     Test mssp accounts search method
     '''
-    with pytest.raises(NotImplementedError):
-        api.v3.vm.folders.search()
+    mock_resp = {
+        'accounts': accounts_us_east
+    }
+    responses.add(
+        responses.POST,
+        f'{MSSP_ACCOUNTS_URL}/search',
+        json=mock_resp
+    )
+    fields = ['id', 'container_name', 'region', 'licensed_assets',
+              'licensed_assets_limit', 'licensed_apps', 'notes', 'logo_uuid']
+    resp = api.v3.mssp.accounts.search(fields=fields,
+                                       filter=('region', 'eq', 'US EAST'),
+                                       sort=[{
+                                           'property': 'container_name',
+                                           'order': 'asc'
+                                       }])
+    actual = []
+    for account in resp:
+        actual.append(account)
+
+    expected = sorted(accounts_us_east, key=lambda i: i['container_name'])
+    assert actual == expected
