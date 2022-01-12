b27c7011807f5c9c6dfe5fc70f62fcebdec57712
parvdesai-tenable <padesai.ctr@tenable.com>
2022-01-12 14:37:57 +0530

    search method

--- tenable/io/v3/mssp/accounts/api.py
+++ tenable/io/v3/mssp/accounts/api.py
@@ -25,18 +25,15 @@ class AccountsAPI(ExploreBaseEndpoint):
 
     def search(self, **kwargs) -> List:
         '''
-        Retrieves the assets.
-
+        Search and retrieve the accounts based on supported conditions.
         Args:
-            fields (list):
+            fields (list, optional):
                 The list of field names to return from the Tenable API.
-
                 Example:
                     - ``['field1', 'field2']``
-            filter (tuple, Dict):
+            filter (tuple, Dict, optional):
                 A nestable filter object detailing how to filter the results
                 down to the desired subset.
-
                 Examples:
                     >>> ('or', ('and', ('test', 'oper', '1'),
                                    ('test', 'oper', '2')
@@ -53,54 +50,47 @@ class AccountsAPI(ExploreBaseEndpoint):
                         {'value': '3', 'operator': 'oper', 'property': 3}
                         ]
                     }
-
                 As the filters may change and sortable fields may change over
                 time, it's highly recommended that you look at the output of
-                the :py:meth:`tio.v3.vm.filters.asset_filters()`
+                the :py:meth:`tio.v3.mssp.filters.accounts()`
                 endpoint to get more details.
-            sort list(tuple, Dict):
+            sort (list[tuple], optional):
                 A list of dictionaries describing how to sort the data
                 that is to be returned.
-
                 Examples:
                     - ``[('field_name_1', 'asc'),
                              ('field_name_2', 'desc')]``
-                    - ``[{'property': 'last_observed', 'order': 'desc'}]``
-            limit (int):
+            limit (int, optional):
                 Number of objects to be returned in each request.
-                Default is 1000.
-            next (str):
+                Default and max_limit is 200.
+            next (str, optional):
                 The pagination token to use when requesting the next page of
-                results.  This token is presented in the previous response.
-            return_resp (bool):
+                results. This token is presented in the previous response.
+            return_resp (bool, optional):
                 If set to true, will override the default behavior to return
                 an iterable and will instead return the results for the
                 specific page of data.
-            return_csv (bool):
-                If set to true, It wil return the CSV Iterable. Returns all
-                data in text/csv format on each next call with row headers
-                on each page.
-
+            return_csv (bool, optional):
+                If set to true, it will return the CSV response or
+                iterable (based on return_resp flag). Iterator returns all
+                rows in text/csv format for each call with row headers.
         Returns:
-            Returns:
-                Iterable:
-                    The iterable that handles the pagination and potentially
-                    async requests for the job.
-                requests.Response:
-                    If ``return_json`` was set to ``True``, then a response
-                    object is instead returned instead of an iterable.
-
+            Iterable:
+                The iterable that handles the pagination for the job.
+            requests.Response:
+                If ``return_json`` was set to ``True``, then a response
+                object is instead returned instead of an iterable.
         Examples:
-            >>> tio.v3.mssp.accounts.search(filter=('netbios_name', 'eq',
-            ...  'SCCM'), fields=['name', 'netbios_name', 'last_login'],
-            ...    limit=2, sort=[('last_observed', 'asc')])
+            >>> tio.v3.mssp.accounts.search(filter=('container_name', 'eq',
+            ...     'Example'), fields=['id', 'container_name', 'region'],
+            ...     limit=2, sort=[('container_name': 'desc')])
         '''
         iclass = SearchIterator
         if kwargs.get('return_csv', False):
             iclass = CSVChunkIterator
-        return super().search(resource='accounts',
-                              iterator_cls=iclass,
-                              is_sort_with_prop=False,
-                              api_path=f'{self._path}/search',
-                              **kwargs
-                              )
+        return super()._search(resource='accounts',
+                               iterator_cls=iclass,
+                               is_sort_with_prop=False,
+                               api_path=f'{self._path}/search',
+                               **kwargs
+                               )
