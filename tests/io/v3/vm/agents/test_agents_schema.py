793ec5f838eea8554e87f86615b63813eb1dfe0e
Milan Aghera <maghera.ctr@tenable.com>
2022-01-28 17:57:25 +0530

    search endpoint support, v3 response changes

--- tests/io/v3/vm/agents/test_agents_schema.py
+++ tests/io/v3/vm/agents/test_agents_schema.py
@@ -4,23 +4,73 @@ Testing the Agents schemas
 from tenable.io.v3.vm.agents.schema import AgentSchema
 
 
-def test_agent_groups_schema_with_name():
+def test_agent_schema():
     '''
     Test the agents schema with name
     '''
+    schema = AgentSchema()
+
+    # Test for schema validation for unlink method
     payload = {
         'items': [
             '57b74c0a-5d95-11ec-bf63-0242ac130002',
-            '57b74e58-5d95-11ec-bf63-0242ac130002',
-            '57b74f66-5d95-11ec-bf63-0242ac130002'
+            '57b74e58-5d95-11ec-bf63-0242ac130002'
         ]
     }
-    test_resp = {
-        'items': [
-            '57b74c0a-5d95-11ec-bf63-0242ac130002',
-            '57b74e58-5d95-11ec-bf63-0242ac130002',
-            '57b74f66-5d95-11ec-bf63-0242ac130002'
-        ]
+    assert payload == schema.dump(schema.load(payload))
+
+    # Test for schema validation for list agent from group method
+    payload = {
+        'filters': [('platform', 'match', ['window'])],
+        'filter_type': 'and',
+        'limit': 50,
+        'offset': 0,
+        'sort': [('name', 'asc'), ('platform', 'asc')],
+        'wildcard': 'IYKKQIGOBWXUXOZIBURFENPNMGZOSWBUKVCD',
+        'wildcard_fields': ['name']
     }
-    schema = AgentSchema(only=['items'])
-    assert test_resp == schema.dump(schema.load(payload))
+    res = {
+        'sort': [('name', 'asc'), ('platform', 'asc')],
+        'offset': 0,
+        'filters': [
+            {
+                'value': ['window'],
+                'operator': 'match',
+                'field': 'platform'
+            }
+        ],
+        'wildcard': 'IYKKQIGOBWXUXOZIBURFENPNMGZOSWBUKVCD',
+        'limit': 50,
+        'filter_type': 'and',
+        'wildcard_fields': ['name']
+    }
+    assert res == schema.dump(schema.load(payload))
+
+    # Test for schema validation for add or remove agent to network method
+    payload = {
+        'criteria': {
+            'all_agents': True,
+            'wildcard': 'sdfknskdnf',
+            'filters': ['name:match:laptop'],
+            'filter_type': 'and',
+            'hardcoded_filters': ['core_version:lt:10.0.0']
+        },
+        'items': ['334b962a-ac03-4336-9ebb-a06b169576e0'],
+        'not_items': ['334b962a-ac03-4336-9ebb-a06b321576e0']
+    }
+    assert payload == schema.dump(schema.load(payload))
+
+    # Test for schema validation for send instruction to agent method
+    payload = {
+        'criteria': {
+            'all_agents': True,
+            'wildcard': 'sdfknskdnf',
+            'filters': ['name:match:laptop'],
+            'filter_type': 'and',
+            'hardcoded_filters': ['core_version:lt:10.0.0']
+        },
+        'items': ['334b962a-ac03-4336-9ebb-a06b169576e0'],
+        'not_items': ['334b962a-ac03-4336-9ebb-a06b321576e0'],
+        'directive': {'type': 'restart'}
+    }
+    assert payload == schema.dump(schema.load(payload))
