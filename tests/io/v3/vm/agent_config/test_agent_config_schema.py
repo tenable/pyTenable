d0e2dc04a460f018fc5659a87acf433aba269206
Milan Aghera <maghera.ctr@tenable.com>
2022-01-31 23:21:52 +0530

    Update schema test cases to improve code coverage

--- tests/io/v3/vm/agent_config/test_agent_config_schema.py
+++ tests/io/v3/vm/agent_config/test_agent_config_schema.py
@@ -8,14 +8,37 @@ def test_agent_config_schema():
     '''
     Test case for agent config schema
     '''
-    payload: dict = {
+    schema = AgentConfigSchema()
+
+    # Schema validation if expiration is greater then one
+    input_payload: dict = {
+        'software_update': True,
+        'auto_unlink': {
+            'expiration': 1
+        }
+    }
+    output_payload: dict = {
         'software_update': True,
         'auto_unlink': {
             'enabled': True,
             'expiration': 1
         }
     }
+    res_payload = schema.dump(schema.load(input_payload))
+    assert output_payload == res_payload
 
-    schema = AgentConfigSchema()
-
-    assert payload == schema.dump(schema.load(payload))
+    # Schema validation if expiration is False or zero
+    input_payload: dict = {
+        'software_update': True,
+        'auto_unlink': {
+            'expiration': 0
+        }
+    }
+    output_payload = {
+        'software_update': True,
+        'auto_unlink': {
+            'enabled': False,
+        }
+    }
+    res_payload = schema.dump(schema.load(input_payload))
+    assert output_payload == res_payload
