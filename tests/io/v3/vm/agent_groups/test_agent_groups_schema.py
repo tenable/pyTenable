62571648b85508513ec5277293b82324a6d6dbf6
Milan Aghera <maghera.ctr@tenable.com>
2022-01-31 22:47:24 +0530

    added search and details endpoint, added new endpoint related to bulk operation API

--- tests/io/v3/vm/agent_groups/test_agent_groups_schema.py
+++ tests/io/v3/vm/agent_groups/test_agent_groups_schema.py
@@ -4,38 +4,65 @@ Testing the AgentGroups schemas
 from tenable.io.v3.vm.agent_groups.schema import AgentGroupSchema
 
 
-def test_agent_groups_schema_with_name():
+def test_agent_groups_schema():
     '''
     Test the agent_groups schema with name
     '''
-    name: str = 'sample name'
-    payload = {
-        'name': name
-    }
-    test_resp = {
-        'name': name
-    }
     schema = AgentGroupSchema()
-    assert test_resp == schema.dump(schema.load(payload))
 
+    # Test for schema validation for create/configure agent method
+    payload = {
+        'name': 'Sample Agent Groups'
+    }
+    assert payload == schema.dump(schema.load(payload))
 
-def test_agent_groups_schema_with_agent_ids():
-    '''
-    Test the agent_groups schema with agent_ids
-    '''
+    # Test for schema validation for add/delete_agents method
     payload = {
         'items': [
             '57b74c0a-5d95-11ec-bf63-0242ac130002',
-            '57b74e58-5d95-11ec-bf63-0242ac130002',
-            '57b74f66-5d95-11ec-bf63-0242ac130002'
+            '57b74e58-5d95-11ec-bf63-0242ac130002'
         ]
     }
-    test_resp = {
-        'items': [
-            '57b74c0a-5d95-11ec-bf63-0242ac130002',
-            '57b74e58-5d95-11ec-bf63-0242ac130002',
-            '57b74f66-5d95-11ec-bf63-0242ac130002'
-        ]
+    assert payload == schema.dump(schema.load(payload))
+
+    # Test for schema validation for details method
+    payload = {
+        'filters': [('platform', 'match', ['window'])],
+        'filter_type': 'and',
+        'limit': 50,
+        'offset': 0,
+        'sort': [('name', 'asc'), ('platform', 'asc')],
+        'wildcard': 'IYKKQIGOBWXUXOZIBURFENPNMGZOSWBUKVCD',
+        'wildcard_fields': ['name']
     }
-    schema = AgentGroupSchema()
-    assert test_resp == schema.dump(schema.load(payload))
+    res = {
+        'sort': [('name', 'asc'), ('platform', 'asc')],
+        'offset': 0,
+        'filters': [
+            {
+                'value': ['window'],
+                'operator': 'match',
+                'field': 'platform'
+            }
+        ],
+        'wildcard': 'IYKKQIGOBWXUXOZIBURFENPNMGZOSWBUKVCD',
+        'limit': 50,
+        'filter_type': 'and',
+        'wildcard_fields': ['name']
+    }
+    assert res == schema.dump(schema.load(payload))
+
+    # Test for schema validation for send_instruction_to_agents_in_group method
+    payload = {
+        'criteria': {
+            'all_agents': True,
+            'wildcard': 'sdfknskdnf',
+            'filters': ['name:match:laptop'],
+            'filter_type': 'and',
+            'hardcoded_filters': ['core_version:lt:10.0.0']
+        },
+        'items': ['334b962a-ac03-4336-9ebb-a06b169576e0'],
+        'not_items': ['334b962a-ac03-4336-9ebb-a06b321576e0'],
+        'directive': {'type': 'restart'}
+    }
+    assert payload == schema.dump(schema.load(payload))
