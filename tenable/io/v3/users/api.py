3aa986a56a61d287f8bf9fb7906a30ee3b22d806
sahils-tenable <ssingh.ctr@tenable.com>
2022-02-07 10:01:08 +0530

    updated definitions doc

--- tenable/io/v3/users/api.py
+++ tenable/io/v3/users/api.py
@@ -369,7 +369,8 @@ class UsersAPI(ExploreBaseEndpoint):
                     ... }
                 As the filters may change and sortable fields may change over
                 time, it's highly recommended that you look at the output of
-                endpoint to get more details.
+                the :py:meth: `tio.v3.definitions.users()` endpoint to
+                get more details.
             sort (list[tuple], optional):
                 sort is a list of tuples in the form of
                 ('FIELD', 'ORDER').
