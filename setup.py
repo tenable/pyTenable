551c8eb676e4f0a2c079f80175660532e7f12de6
Ted O'Meara <tomeara@tenable.com>
2022-03-07 15:54:50 -0500

    upgrades restfly to 1.4.5
    Type of change: dependency
    
    Goals:
    * Remove the error when Content-Type casing causes errors
    
    How does this work towards the goals:
    * Upgrades restfly as noted here: https://github.com/tenable/pyTenable/issues/520

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'requests>=2.26',
         'python-dateutil>=2.6',
         'semver>=2.8.1',
-        'restfly>=1.4.4',
+        'restfly>=1.4.5',
         'marshmallow>=3.6',
         'python-box>=4.0',
         'defusedxml>=0.5.0',
