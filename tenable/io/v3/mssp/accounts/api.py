30c479fb1aaf165a4d3ba40fc6750885d244318a
Alpesh Sorathiya <Alpesh_Sorathiya@musikaar.com>
2022-03-10 16:11:16 +0530

    updated do string

--- tenable/io/v3/mssp/accounts/api.py
+++ tenable/io/v3/mssp/accounts/api.py
@@ -95,8 +95,8 @@ class AccountsAPI(ExploreBaseEndpoint):
             Iterable:
                 The iterable that handles the pagination for the job.
             requests.Response:
-                If ``return_json`` was set to ``True``, then a response
-                object is instead returned instead of an iterable.
+                If ``return_resp`` is set to ``True``, then a response
+                object is returned instead of an iterable.
         Examples:
             >>> tio.v3.mssp.accounts.search(filter=('container_name', 'eq',
             ...     'Example'), fields=['id', 'container_name', 'region'],
