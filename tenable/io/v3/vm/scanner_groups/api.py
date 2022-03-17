4ea985c138d5973111f2766b684387c33e7cfeaa
Alpesh sorathiya <Alpesh-te@musikaar.com>
2022-03-17 12:10:47 +0530

    updated do string

--- tenable/io/v3/vm/scanner_groups/api.py
+++ tenable/io/v3/vm/scanner_groups/api.py
@@ -239,8 +239,8 @@ class ScannerGroupsAPI(ExploreBaseEndpoint):
             Iterable:
                 The iterable that handles the pagination for the job.
             requests.Response:
-                If ``return_json`` was set to ``True``, then a response
-                object is instead returned instead of an iterable.
+                If ``return_resp`` is set to ``True``, then a response
+                object is returned instead of an iterable.
         Examples:
             >>> tio.v3.vm.scanner_groups.search(
             ...     filter=('name','eq','SCCM'),
