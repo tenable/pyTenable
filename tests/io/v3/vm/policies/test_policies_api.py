c626cb7bf6021ab966795744e8c3a3a89b9fa3ff
Alpesh sorathiya <Alpesh-te@musikaar.com>
2022-03-25 11:09:18 +0530

    rid of looping

--- tests/io/v3/vm/policies/test_policies_api.py
+++ tests/io/v3/vm/policies/test_policies_api.py
@@ -340,10 +340,7 @@ def test_search(api):
     )
     assert isinstance(iterator, SearchIterator)
 
-    exclusions_list = []
-    for item in iterator:
-        exclusions_list.append(item)
-    assert len(exclusions_list) == test_response['pagination']['total']
+    assert len(list(iterator)) == test_response['pagination']['total']
 
     iterator = api.v3.vm.policies.search(
         fields=fields, filter=filter, sort=sort, return_csv=True
