ecdcc4069bf2ec3d58652eb43e2b769934b44c30
AG <github@mzpqnxow.com>
2022-04-15 09:34:46 -0400

    Update tenable/reports/nessusv2.py
    This makes sense to me
    
    Co-authored-by: Steven McGrath <steve@mcgrath.sh>

--- tenable/reports/nessusv2.py
+++ tenable/reports/nessusv2.py
@@ -54,9 +54,8 @@ class NessusReportv2(object):
 
         elif name in ['cvss_base_score', 'cvss_temporal_score']:
             # CVSS scores are floats, so lets return them as such.
-            if value is None:  # Issue #551
-                return 0
-            return float(value)
+            if value:
+                return float(value)
 
         elif name in ['first_found', 'last_found', 'plugin_modification_date',
                       'plugin_publication_date', 'HOST_END', 'HOST_START']:
