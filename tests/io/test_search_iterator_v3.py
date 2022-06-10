295a36c89e580120404f94aacbda43b57f20f90d
Mitali Upadhyay <Mitali_Upadhyay@musikaar.com>
2022-06-10 17:02:24 +0530

    Updated search iterator test-case for v3

--- tests/io/test_search_iterator_v3.py
+++ tests/io/test_search_iterator_v3.py
@@ -6,27 +6,19 @@ import pytest
 from tenable.io.v3.base.iterators.explore_iterator import SearchIterator
 
 ASSET_DATA = [
-    {'ipv6_addresses': [], 'netbios_name': 'DC1', 'sources': ['NESSUS_SCAN'],
-     'last_licensed_scan_time': '2022-05-25T12:20:45.000Z', 'ipv4_addresses': ['172.26.48.10'],
-     'aes': {'score': 755, 'is_predicted': True, 'confidence': 50.0},
-     'network': {'id': '00000000-0000-0000-0000-000000000000', 'name': 'Default'},
-     'display_ipv4_address': '172.26.48.10', 'first_observed': '2022-05-25T12:20:45.000Z',
-     'acr': {'score': 6, 'calculated_score': 6, 'drivers': [{'values': ['general_server'], 'name': 'device_type'},
-                                                            {'values': ['dns_server', 'directory_server'],
-                                                             'name': 'device_capability'},
-                                                            {'values': ['internal'], 'name': 'internet_exposure'}]},
-     'is_deleted': False, 'last_observed': '2022-05-25T12:20:45.000Z', 'id': '88542bdc-7a3e-4b01-8ee3-87c09726eec5',
-     'types': ['host'], 'created': '2022-05-25T12:20:47.346Z', 'observation_sources': [
-        {'first_observed': '2022-05-25T12:20:45.000Z', 'last_observed': '2022-05-25T12:20:45.000Z',
-         'name': 'NESSUS_SCAN'}], 'is_licensed': True, 'fqdns': ['dc1.target.tenablesecurity.com'], 'tags': [
-        {'id': 'd74355e4-be1e-461b-ad7d-b842df740330', 'category': 'Target Groups', 'value': 'ATL PROD',
-         'type': 'dynamic'},
-        {'id': 'dc01ec2a-4872-4f09-8388-4538f43aa186', 'category': 'Test1', 'value': '11', 'type': 'dynamic'}],
-     'operating_systems': ['Microsoft Windows Server 2008 R2 Datacenter Service Pack 1'],
-     'display_fqdn': 'dc1.target.tenablesecurity.com',
-     'display_operating_system': 'Microsoft Windows Server 2008 R2 Datacenter Service Pack 1',
-     'system_type': 'general-purpose', 'installed_software': ['cpe:/a:microsoft:iis:7.5'], 'is_public': False,
-     'name': 'dc1', 'updated': '2022-06-09T19:38:32.869Z', 'host_name': 'dc1'}
+    {'types': ['webapp'], 'is_deleted': False, 'sources': ['WAS'], 'ssl_tls_enabled': False,
+     'created': '2022-01-25T15:19:17.224Z', 'name': 'target9.pubtarg.bananas.com', 'is_licensed': False,
+     'id': '0f19a1a4-d1df-4f8a-a2ec-9cd5f4f3f3c5', 'updated': '2022-01-25T15:21:29.692Z',
+     'tags': [{'id': '6167eae5-808a-415a-8400-f3208e5382c5', 'category': 'ABCD', 'value': 'tag111', 'type': 'static'}]},
+    {'types': ['webapp'], 'sources': ['WAS'], 'last_licensed_scan_time': '2022-01-25T15:17:50.322Z',
+     'created': '2021-10-28T13:47:40.801Z', 'ipv4_addresses': ['44.241.194.21'],
+     'homepage_screenshot_url': 'https://cloud.tenable.com/was/v2/attachments/54fd0e88-7377-4755-8e9b-7b944f671de9',
+     'tags': [{'id': '6167eae5-808a-415a-8400-f3208e5382c5', 'category': 'ABCD', 'value': 'tag111', 'type': 'static'}],
+     'display_ipv4_address': '44.241.194.21', 'first_observed': '2021-10-28T13:47:40.694Z',
+     'display_operating_system': 'Linux Kernel 3.10 on CentOS Linux release 7', 'ssl_tls_enabled': False,
+     'last_observed': '2022-01-25T15:17:50.322Z', 'name': 'target1.pubtarg.tenablesecurity.com',
+     'id': '8a148a61-7f5f-4730-8ac0-ebcebcbd6a4d', 'updated': '2022-01-25T15:22:22.966Z'}
+
 ]
 
 
@@ -64,9 +56,8 @@ def test_search_iterator_v3(api):
             }
         }
     )
-    print(next(search_iterator))
-    print(next(search_iterator))
-    # assert ASSET_DATA.__contains__(next(search_iterator))
+    assert ASSET_DATA.__contains__(next(search_iterator))
+    assert ASSET_DATA.__contains__(next(search_iterator))
 
-    # with pytest.raises(StopIteration):
-    #     next(search_iterator)
+    with pytest.raises(StopIteration):
+        next(search_iterator)
