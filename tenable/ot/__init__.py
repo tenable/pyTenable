168762a64d6c767a36d654c928188389f2f11b62
Adam Delman <adelman@tenable.com>
2022-09-08 14:32:05 +0300

    Add more support for Tenable OT asset details  - Also refactor Tenable OT GraphQL api classes

--- tenable/ot/__init__.py
+++ tenable/ot/__init__.py
@@ -1,4 +1,20 @@
-'''
-Tenable.ot Base Package
-'''
+"""
+Tenable.ot base package
+=======================
+
+This package covers the Tenable.ad interface.
+
+.. autoclass:: TenableOT
+    :members:
+
+
+.. toctree::
+    :hidden:
+    :glob:
+
+    assets
+    events
+    plugins
+"""
+
 from .session import TenableOT
