7ceed2bf4e2aabedab873cbfe808988896f21920
Aseem Savio <aseemsavio3@gmail.com>
2022-10-28 08:01:16 +0530

    added back search_cloud_resource and search_webapp for assets and findings v3 APIs

--- tests/io/test_assets_api_v3.py
+++ tests/io/test_assets_api_v3.py
@@ -51,3 +51,73 @@ def test_explore_assets_v3_search_all_assets(api):
                                                            return_resp=True, return_csv=True)
     assert isinstance(assets_csv_response, Response), "Response object (CSV format) is not returned in response of " \
                                                       "Explore -> All Assets with return_resp flag enabled"
+
+
+@pytest.mark.vcr()
+def test_explore_assets_v3_search_was_assets(api):
+    '''
+    Test the search web applications assets endpoint
+    '''
+    tag_list = ['522b0827-1780-42c5-9f70-2e481a845b41']
+    sort = [('created', 'desc')]
+    filters = ('and', ('last_observed', 'lt', '2022-05-14T18:30:00.000Z'),
+               ('tags', 'eq', tag_list))
+
+    assets_iterator = api.v3.explore.assets.search_webapp(limit=200, sort=sort, filter=filters)
+    assert isinstance(assets_iterator, SearchIterator), \
+        "Iterator is not returned in response of Explore -> Web Applications Assets"
+
+    for asset_data in assets_iterator:
+        for asset_tag in asset_data['tags']:
+            assert asset_tag['id'] in tag_list, \
+                "Web Application Assets are not filtered as per 'tags' filter criteria."
+
+    assets_csv_iterator = api.v3.explore.assets.search_webapp(sort=sort, limit=200, filter=filters, return_csv=True)
+    assert isinstance(assets_csv_iterator, CSVChunkIterator), \
+        "CSV chunk iterator is not returned in response of " \
+        "Explore -> Web Applications Assets with return_csv flag enabled"
+
+    assets_response = api.v3.explore.assets.search_webapp(limit=200, sort=sort, filter=filters, return_resp=True)
+    assert isinstance(assets_response, Response), "Response object is not returned in response of " \
+                                                  "Explore -> Web Applications Assets with return_resp flag enabled"
+
+    assets_csv_response = api.v3.explore.assets.search_webapp(limit=200, sort=sort, filter=filters, return_resp=True,
+                                                              return_csv=True)
+    assert isinstance(assets_csv_response, Response), "Response object (CSV format) is not returned in response of " \
+                                                      "Explore -> Web Applications Assets with return_resp flag enabled"
+
+
+@pytest.mark.vcr()
+def test_explore_assets_v3_search_cloud_resource_assets(api):
+    '''
+    Test the search cloud resource assets endpoint
+    '''
+    asset_name_list = ['CSTest', 'CSTest-ip']
+    region_list = ['CSTest_group']
+    sort = [('updated', 'desc')]
+    filters = ('and', ('name', 'eq', asset_name_list), ('cloud.runtime.region', 'eq', region_list))
+
+    assets_iterator = api.v3.explore.assets.search_cloud_resource(limit=5, sort=sort, filter=filters)
+    assert isinstance(assets_iterator, SearchIterator), \
+        "Iterator is not returned in response of Explore -> Cloud Resources Assets"
+
+    for asset_data in assets_iterator:
+        assert asset_data['name'] in asset_name_list, \
+            "Cloud Resource Assets are not filtered as per 'name' filter criteria."
+        assert asset_data['cloud']['runtime']['region'] in region_list, \
+            "Cloud Resource Assets are not filtered as per 'region' filter criteria."
+
+    assets_csv_iterator = api.v3.explore.assets.search_cloud_resource(sort=sort, limit=5, filter=filters,
+                                                                      return_csv=True)
+    assert isinstance(assets_csv_iterator, CSVChunkIterator), \
+        "CSV chunk iterator is not returned in response of " \
+        "Explore -> Cloud Resources Assets with return_csv flag enabled"
+
+    assets_response = api.v3.explore.assets.search_cloud_resource(limit=5, sort=sort, filter=filters, return_resp=True)
+    assert isinstance(assets_response, Response), "Response object is not returned in response of " \
+                                                  "Explore -> Cloud Resources Assets with return_resp flag enabled"
+
+    assets_csv_response = api.v3.explore.assets.search_cloud_resource(limit=5, sort=sort, filter=filters,
+                                                                      return_resp=True, return_csv=True)
+    assert isinstance(assets_csv_response, Response), "Response object (CSV format) is not returned in response of " \
+                                                      "Explore -> Cloud Resources Assets with return_resp flag enabled"
--- tests/io/test_findings_api_v3.py
+++ tests/io/test_findings_api_v3.py
@@ -40,3 +40,75 @@ def test_findings_v3_search_host(api):
     )
     assert isinstance(resp, Response), "Response object (CSV format) is not returned in response of " \
                                        "Explore -> Host Findings with return_resp & return_csv flag enabled"
+
+
+@pytest.mark.vcr()
+def test_findings_v3_search_was(api):
+    '''
+    Test the search endpoint of web applications - findings
+    '''
+
+    fields = ['finding_id', 'severity', 'state', 'last_observed']
+    sort = [("last_observed", "desc")]
+    filter = ('and', ('severity', 'eq', [3]),
+              ('state', 'eq', ['ACTIVE']))
+
+    iterator = api.v3.explore.findings.search_webapp(
+        fields=fields, filter=filter, sort=sort, limit=200,
+    )
+    assert isinstance(iterator, SearchIterator), \
+        "Iterator is not returned in response of Explore -> Web Applications Findings"
+
+    iterator = api.v3.explore.findings.search_webapp(
+        fields=fields, filter=filter, return_csv=True, sort=sort, limit=200,
+    )
+    assert isinstance(iterator, CSVChunkIterator), "CSV chunk iterator is not returned in response of " \
+                                                   "Explore -> Web Applications Findings with return_csv flag enabled"
+
+    resp = api.v3.explore.findings.search_webapp(
+        fields=fields, filter=filter, return_resp=True, sort=sort, limit=200
+    )
+    assert isinstance(resp, Response), "Response is not returned in response of " \
+                                       "Explore -> Web Applications Findings with return_resp flag enabled"
+
+    resp = api.v3.explore.findings.search_webapp(
+        fields=fields, filter=filter, return_resp=True, return_csv=True, sort=sort, limit=200
+    )
+    assert isinstance(resp, Response), "Response object (CSV format) is not returned in response of " \
+                                       "Explore -> Web Applications Findings with return_resp & return_csv flag enabled"
+
+
+@pytest.mark.vcr()
+def test_findings_v3_search_cloud_resource(api):
+    '''
+    Test the search endpoint of cloud resource - findings
+
+    '''
+
+    fields = ['finding_id', 'last_found_time', 'risk_factor_num', 'asset.name', 'state']
+    sort = [("state", "desc")]
+    filter = ('and', ('risk_factor_num', 'eq', 3), ('state', 'eq', ['ACTIVE']))
+
+    iterator = api.v3.explore.findings.search_cloud_resource(
+        fields=fields, filter=filter, sort=sort, limit=200,
+    )
+    assert isinstance(iterator, SearchIterator), \
+        "Iterator is not returned in response of Explore -> Cloud Findings"
+
+    iterator = api.v3.explore.findings.search_cloud_resource(
+        fields=fields, filter=filter, return_csv=True, sort=sort, limit=200,
+    )
+    assert isinstance(iterator, CSVChunkIterator), "CSV chunk iterator is not returned in response of " \
+                                                   "Explore -> Cloud Findings with return_csv flag enabled"
+
+    resp = api.v3.explore.findings.search_cloud_resource(
+        fields=fields, filter=filter, return_resp=True, return_csv=True, sort=sort, limit=200
+    )
+    assert isinstance(resp, Response), "Response object (CSV format) is not returned in response of " \
+                                       "Explore -> Cloud Findings with return_resp & return_csv flag enabled"
+
+    resp = api.v3.explore.findings.search_cloud_resource(
+        fields=fields, filter=filter, return_resp=True, sort=sort, limit=200
+    )
+    assert isinstance(resp, Response), "Response is not returned in response of " \
+                                       "Explore -> Cloud Findings with return_resp flag enabled"
