9208ff555b8deaea2cdf61cdf78fc80ccf1fd2b3
Aseem Savio <aseemsavio3@gmail.com>
2023-03-24 16:35:41 +0530

    Test to ensure iterator doesn't break on no data found

--- tests/io/was/test_api.py
+++ tests/io/was/test_api.py
@@ -20,3 +20,20 @@ def test_was_export(api: TenableIO):
 
     assert isinstance(findings_iterator, WasIterator)
     assert len(findings) > 0
+
+@pytest.mark.vcr()
+def test_ensure_iterator_does_not_break_on_no_data_found(api: TenableIO):
+    """
+    Test to ensure iterator doesn't break on no data found
+    """
+    findings_iterator = api.was.export(
+        and_filter=[
+            ("scans_started_at", "gte", "2100/03/23"),
+            ("scans_status", "contains", ["completed"])
+        ]
+    )
+
+    findings = [finding for finding in findings_iterator]
+
+    assert isinstance(findings_iterator, WasIterator)
+    assert len(findings) == 0
