efa3e2d9b66f01961303f55496e96aed7c7445d9
Aseem Savio <aseemsavio3@gmail.com>
2023-12-11 16:32:08 +0530

    edit and delete api

--- tenable/sc/was_scan.py
+++ tenable/sc/was_scan.py
@@ -19,7 +19,7 @@ from typing import List, Optional, Any
 from tenable.sc.base import SCEndpoint
 
 
-def _boolean_string(boolean_value: bool) -> str:
+def _to_boolean_string(boolean_value: bool) -> str:
     """
     Returns a boolean string representation of a python Boolean value.
     True returns "true", False returns "false"
@@ -36,7 +36,7 @@ def _boolean_string(boolean_value: bool) -> str:
 class WasScanAPI(SCEndpoint):
     api_route = "wasScan"
 
-    def _build_creation_request(self, **kwargs: Any) -> dict:
+    def _build_creation_or_update_request(self, **kwargs: Any) -> dict:
         """
         Builds and returns the request object for an SC WAS Scan creation.
         Note: we're converting numbers to strings in this function as the API requires it to be a string.
@@ -67,7 +67,7 @@ class WasScanAPI(SCEndpoint):
             }
 
         if "dhcpTracking" in kwargs:
-            request["dhcpTracking"] = _boolean_string(kwargs["dhcpTracking"])
+            request["dhcpTracking"] = _to_boolean_string(kwargs["dhcpTracking"])
 
         if "classifyMitigatedAge" in kwargs:
             request["classifyMitigatedAge"] = str(kwargs["classifyMitigatedAge"])
@@ -109,10 +109,10 @@ class WasScanAPI(SCEndpoint):
                 ]
 
         if "emailOnLaunch" in kwargs:
-            request["emailOnLaunch"] = _boolean_string(kwargs["emailOnLaunch"])
+            request["emailOnLaunch"] = _to_boolean_string(kwargs["emailOnLaunch"])
 
         if "emailOnFinish" in kwargs:
-            request["emailOnFinish"] = _boolean_string(kwargs["emailOnFinish"])
+            request["emailOnFinish"] = _to_boolean_string(kwargs["emailOnFinish"])
 
         if "timeoutAction" in kwargs:
             request["timeoutAction"] = self._check("timeoutAction",
@@ -122,7 +122,7 @@ class WasScanAPI(SCEndpoint):
                                                    )
 
         if "scanningVirtualHosts" in kwargs:
-            request["scanningVirtualHosts"] = _boolean_string(kwargs["scanningVirtualHosts"])
+            request["scanningVirtualHosts"] = _to_boolean_string(kwargs["scanningVirtualHosts"])
 
         if "rolloverType" in kwargs:
             request["rolloverType"] = self._check("rolloverType",
@@ -191,7 +191,7 @@ class WasScanAPI(SCEndpoint):
         Examples:
             >>> zone = sc.was_scan.create(name="Example Scan")
         """
-        request = self._build_creation_request(**kwargs)
+        request = self._build_creation_or_update_request(**kwargs)
         return self._api.post(self.api_route, json=request).json()['response']
 
     def list(self, fields: Optional[List[str]] = None):
@@ -219,37 +219,106 @@ class WasScanAPI(SCEndpoint):
 
         return self._api.get(self.api_route, params=params).json()['response']
 
-    def details(self, id: Optional[str] = None, uuid: Optional[str] = None, fields: Optional[List[str]] = None) -> dict:
+    def details(self, scan_id: Optional[str] = None, fields: Optional[List[str]] = None) -> dict:
         """
-        Retrieves the details of the given WAS Scan. Either id or the uuid should be present
+        Retrieves the details of the given WAS Scan.
 
         :sc-api:`was-scan: list <Was-Scan.htm#WASScanRESTReference-/wasScan/{id}>`
 
         Args:
-            id (str, optional):
+            scan_id (str, optional):
                 ID of the WAS scan to be fetched.
-            uuid (str, optional):
-                UUID of the WAS scan to be fetched.
             fields (list, optional):
                 A list of attributes to return for the scan.
 
         Returns:
-            :obj:`list`:
-                A list of WAS Scans.
+            :obj:`dict`:
+                The details of the WAS scan.
 
         Examples:
             >>> sc.was_scan.details(1)
         """
-        if (id is not None and uuid is not None) or (id is None and uuid is None):
-            raise ValueError(
-                "You can only provide id OR UUID, and not both. They cannot both be empty at the same time."
-            )
 
         params = {}
         if fields:
             string_fields = [self._check("fields", f, str) for f in fields]
             params["fields"] = ",".join(string_fields)
 
-        identifier = id if id else uuid
+        return self._api.get(f"{self.api_route}/{scan_id}", params=params).json()['response']
 
-        return self._api.get(f"{self.api_route}/{identifier}", params=params).json()['response']
+    def edit(self, scan_id: str, **kwargs) -> dict:
+        """
+        Updates a WAS Scan depending on access and permissions.
+
+        :sc-api:`was-scan: list <Was-Scan.htm#wasScan_uuid_PATCH>`
+
+        Args:
+            scan_id (str):
+                ID of the scan whose parameters need to be edited.
+            name (str):
+                The name of the scan.
+            type (str, optional):
+                Type of the scan.
+            description (str, optional):
+                A description for the scan zone.
+            repository_id (int):
+                Repository ID.
+            zone_id (int, optional):
+                Zone ID.
+            dhcpTracking (boolean, optional):
+                Scould Security Center do DHCP Tracking?
+            classifyMitigatedAge (int, optional):
+                Classify Mitigated Age
+            schedule_type (str, optional):
+                Schedule Type includes  "dependent" | "ical" | "never" | "rollover" | "template"
+            reports (list, optional):
+                This is a List of Tuples of 2 elements. The first element in the tuple should be a number (ID)
+                and the last/second element should be a string (reportSource). It can be one of these strings.
+                "cumulative" | "patched" | "individual" | "lce" | "archive" | "mobile"
+            assets: (list, optional):
+                List of Asset IDs.
+            credentials (list, optional):
+                List of Credential IDs.
+            emailOnLaunch (boolean, optional):
+                Should we send an email on launch?
+            emailOnFinish (boolean, optional):
+                Should we send an email upon completion of the scan?
+            timeoutAction (str, optional):
+                Time out action can be one of these: "discard" | "import" | "rollover"
+            scanningVirtualHosts (boolean, optional):
+                Should we scan Virtual Hosts?
+            rolloverType (str, optional):
+                Rollover type should be one of these: "nextDay" | "template"
+            urlList (str, optional)
+                Valid URL List
+            maxScanTime (int, optional)
+                Max Scan Time
+
+        Returns:
+            :obj:`dict`:
+                The edited WAS Scan
+
+        Examples:
+            >>> zone = sc.was_scan.edit(scan_id=1, name="Example Scan")
+        """
+        request = self._build_creation_or_update_request(**kwargs)
+        return self._api.patch(f"{self.api_route}/{scan_id}", json=request).json()['response']
+
+    def delete(self, scan_id: str) -> dict:
+        """
+        Deletes the given WAS Scan.
+
+        :sc-api:`was-scan: list <Was-Scan.htm#wasScan_uuid_DELETE>`
+
+        Args:
+            scan_id (str, optional):
+                ID of the WAS scan to be fetched.
+
+        Returns:
+            :obj:`dict`:
+                A deletion response.
+
+        Examples:
+            >>> sc.was_scan.delete(scan_id=1)
+        """
+        return self._api.delete(f"{self.api_route}/{scan_id}").json()
