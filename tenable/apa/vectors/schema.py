c66a00b7939ddeb93f868e42d12b77eaeb821094
Naum Raviz <51710563+naumraviz@users.noreply.github.com>
2024-07-23 19:11:54 +0300

    Adding support for GET /api/v1/vectors endpoint (#818)
    * Vectors
    
    * Vectors
    
    * Vectors
    
    * Add test_vectors_api.py
    
    * Add import
    
    * Finalize VectorIterator
    
    * Finish tests
    
    * Style
    
    * Style
    
    * remove print
    
    * FIx

---
+++ tenable/apa/vectors/schema.py
@@ -0,0 +1,51 @@
+from marshmallow import fields, Schema, validates_schema, ValidationError
+
+
+class SourceInformationSchema(Schema):
+    provider_detection_id = fields.Str(allow_none=True)
+    detection_code = fields.Str(allow_none=True)
+    reason_code_name = fields.Str(allow_none=True)
+    asset_id = fields.Str(allow_none=True)
+    id = fields.Str(allow_none=True)
+    provider_code = fields.Str(allow_none=True)
+    type = fields.Str(allow_none=True)
+    reason_id = fields.Str(allow_none=True)
+    plugin_name = fields.Str(allow_none=True)
+
+
+class TechniqueSchema(Schema):
+    source_information = fields.Str(allow_none=True)
+    name = fields.Str(allow_none=True)
+    fullName = fields.Str(allow_none=True)
+    asset_id = fields.Str(allow_none=True)
+    id = fields.Int(allow_none=True)
+    labels = fields.List(fields.Str(), allow_none=True)
+    procedure_uuid = fields.Str(allow_none=True)
+
+
+class NodeSchema(Schema):
+    name = fields.Str(allow_none=True)
+    fullName = fields.Str(allow_none=True)
+    asset_id = fields.Str(allow_none=True)
+    id = fields.Int(allow_none=True)
+    labels = fields.List(fields.Str(), allow_none=True)
+
+
+class VectorSchema(Schema):
+    isNew = fields.Bool(allow_none=True)
+    vectorId = fields.Str(allow_none=True)
+    path = fields.Raw(allow_none=True)
+    techniques = fields.List(fields.Nested(TechniqueSchema), allow_none=True)
+    nodes = fields.List(fields.Nested(NodeSchema), allow_none=True)
+    findingsNames = fields.List(fields.Str(), allow_none=True)
+    name = fields.Str(allow_none=True)
+    summary = fields.Str(allow_none=True)
+    firstAES = fields.Raw(allow_none=True)
+    lastACR = fields.Int(allow_none=True)
+
+
+class VectorsPageSchema(Schema):
+    data = fields.List(fields.Nested(VectorSchema), allow_none=True)
+    pageNumber = fields.Int(allow_none=True)
+    count = fields.Int(allow_none=True)
+    total = fields.Int(allow_none=True)
