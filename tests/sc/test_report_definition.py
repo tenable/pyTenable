2298422be2655bc7b93c348fc818ee71bfef25e6
Sam Marsh <samwmarsh@gmail.com>
2024-11-21 21:40:16 +0000

    add success check

--- tests/sc/test_report_definition.py
+++ tests/sc/test_report_definition.py
@@ -5,10 +5,26 @@ import pytest
 from ..checker import check, single
 
 def test_report_definition_id_typeerror(security_center):
+    '''
+    validate id is int not string
+    '''
     with pytest.raises(TypeError):
         security_center.report_definition.launch(id='one')
 
 def test_report_definition_id_item_typeerror(security_center):
+    '''
+    validate id is int not list
+    '''
+    
     with pytest.raises(TypeError):
         security_center.report_definition.launch(id=['one'])
 
+def test_report_definition_launch_success(security_center):
+    '''
+    validate launch is successful and returns running report ID
+    '''
+    report = security_center.report_definition.launch(id=1)
+    assert isinstance(report, dict)
+    check(report['reportResult'], 'id', int)
+    check(report['reportResult'], 'name', str)
+    check(report['reportResult'], 'status', str)
