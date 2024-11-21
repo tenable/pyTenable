707af9ec14221711f0a0f17ab0a22b3f2719812a
Sam Marsh <samwmarsh@gmail.com>
2024-11-21 20:53:00 +0000

    tests for report definition id being an int

--- tests/sc/test_report_definition.py
+++ tests/sc/test_report_definition.py
@@ -4,11 +4,11 @@ test file to test various scenarios in sc report_definition
 import pytest
 from ..checker import check, single
 
-def test_report_definition_id_typeerror(sc):
+def test_report_definition_id_typeerror(security_center):
     with pytest.raises(TypeError):
-        sc.report_definition.launch(id='one')
+        security_center.report_definition.launch(id='one')
 
-def test_report_definition_id_item_typeerror(sc):
+def test_report_definition_id_item_typeerror(security_center):
     with pytest.raises(TypeError):
-        sc.report_definition.launch(id=['one'])
+        security_center.report_definition.launch(id=['one'])
 
