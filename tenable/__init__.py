3cb064d714ccbb757caa99a40178b8a1da9102da
Steve McGrath <smcgrath@tenable.com>
2025-03-10 16:40:13 -0500

    Updated readthedocs to use uv

--- tenable/__init__.py
+++ tenable/__init__.py
@@ -1,17 +1,13 @@
-'''
+"""
 Welcome to pyTenable's documentation!
 =====================================
 
-.. image:: https://img.shields.io/endpoint.svg?url=https%3A%2F%2Factions-badge.atrox.dev%2Ftenable%2FpyTenable%2Fbadge&label=build
-   :target: https://github.com/tenable/pyTenable/actions
 .. image:: https://img.shields.io/pypi/v/pytenable.svg
-   :target: https://pypi.org/project/pyTenable/
-.. image:: https://img.shields.io/pypi/pyversions/pyTenable.svg
-   :target: https://pypi.org/project/pyTenable/
-.. image:: https://img.shields.io/pypi/dm/pyTenable.svg
-   :target: https://github.com/tenable/pytenable
+.. image:: https://img.shields.io/badge/python-3.10%2B-blue
+.. image:: https://img.shields.io/readthedocs/pytenable
+.. image:: https://img.shields.io/pypi/dm/pytenable
 .. image:: https://img.shields.io/github/license/tenable/pyTenable.svg
-   :target: https://github.com/tenable/pytenable
+.. image:: https://sonarcloud.io/api/project_badges/measure?project=tenable_pyTenable&metric=alert_status
 
 pyTenable is intended to be a pythonic interface into the Tenable application
 APIs.  Further by providing a common interface and a common structure between
@@ -20,12 +16,7 @@ different APIs between some of the products.
 
 - Issue Tracker: https://github.com/tenable/pyTenable/issues
 - Github Repository: https://github.com/tenable/pyTenable
-
-.. note::
-    pyTenable version 1.2 is the first release to drop support for Python
-    versions less than 3.7.  If you are still using any of these deprecated
-    python versions the 1.1 release will still work, however is no longer being
-    actively developed or maintained.
+- Docs: https://pytenable.readthedocs.io
 
 Installation
 ------------
@@ -37,7 +28,7 @@ installing via pip:
 
    pip install pytenable
 
-If your looking for bleeding-edge, then feel free to install directly from the
+If you're looking for bleeding-edge, then feel free to install directly from the
 github repository like so:
 
 .. code-block:: bash
@@ -53,7 +44,7 @@ Tenable.io application.  Performing this action is as simple as the following:
 .. code-block:: python
 
    from tenable.io import TenableIO
-   tio = TenableIO('TIO_ACCESS_KEY', 'TIO_SECRET_KEY')
+   tio = TenableIO(access_key='TIO_ACCESS_KEY', secret_key='TIO_SECRET_KEY')
    for scan in tio.scans.list():
       print('{status}: {id}/{uuid} - {name}'.format(**scan))
 
@@ -62,13 +53,12 @@ Getting started with Tenable.sc is equally as easy:
 .. code-block:: python
 
    from tenable.sc import TenableSC
-   sc = TenableSC('SECURITYCENTER_NETWORK_ADDRESS')
-   sc.login('SC_USERNAME', 'SC_PASSWORD')
+   sc = TenableSC(url='https://SC_URL', access_key='AKEY', secret_key='SKEY')
    for vuln in sc.analysis.vulns():
       print('{ip}:{pluginID}:{pluginName}'.format(**vuln))
 
-For more detailed information on what's available, please refer to the navigation
-section for the Tenable application you're looking
+For more detailed information on whats available, please refer to the
+`pyTenable Documentation <https://pytenable.readthedocs.io/>`_
 
 Logging
 -------
@@ -85,7 +75,9 @@ License
 -------
 
 The project is licensed under the MIT license.
-'''
+"""
+
 from .version import version as __version__
-__author__ = 'Steve McGrath <smcgrath@tenable.com>'
+
+__author__ = 'Tenable, Inc. <support@tenable.com>'
 __license__ = 'MIT'
