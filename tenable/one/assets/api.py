a6b2ad0398e14e0aa883a8cb003e819c19f90184
naumraviz <naumraviz@gmail.com>
2025-03-12 21:36:32 +0200

    Add assets_response

--- tenable/one/assets/api.py
+++ tenable/one/assets/api.py
@@ -17,7 +17,7 @@ from tenable.one.assets.schema import AssetProperties, AssetClass, AssetField
 
 class AssetsAPI(APIEndpoint):
 
-    def list_properties(self, asset_classes: Optional[list[AssetClass]] = None) -> list[AssetField]:
+    def list_properties(self, asset_classes: Optional[list[AssetClass]] = None):
         """
          Retrieve assets properties
 
@@ -44,3 +44,58 @@ class AssetsAPI(APIEndpoint):
             payload["asset_classes"] = asset_classes
         asset_properties_response: dict[str, list[dict]] = self._get(path="one/api/v1/assets/properties", params=payload)
         return AssetProperties(**asset_properties_response).properties
+
+    def list(
+            self,
+            query_text: str,
+            query_mode: str = "simple",
+            filters: Optional[list[dict]] = None,
+            extra_properties: Optional[list[str]] = None,
+            skip: int = 0,
+            max_page_size: int = 100,
+            sort_by: str = "aes",
+            sort_direction: str = "desc",
+            timezone: str = "America/Chicago",
+    ):
+        """
+         Retrieve assets
+
+         Args:
+            query_text (str):
+                The text to search for.
+            query_mode (str, optional):
+                The search mode. Defaults to "simple".
+            filters (list, optional):
+                A list of filters to apply. Defaults to None.
+            extra_properties (list, optional):
+                Additional properties to include in the response. Defaults to None.
+            skip (int, optional):
+                Number of records to skip. Defaults to 0.
+            max_page_size (int, optional):
+                Maximum number of records per page. Defaults to 100.
+            sort_by (str, optional):
+                Field to sort by. Defaults to "aes".
+            sort_direction (str, optional):
+                Sorting direction, either "asc" or "desc". Defaults to "desc".
+            timezone (str, optional):
+                Timezone setting for the query. Defaults to "America/Chicago".
+
+        Returns:
+            :obj:`AssetProperties`:
+                The asset properties.
+
+         Examples:
+             >>> tone_assets = tone.assets.list()
+             >>> for asset_property in tone_asset_properties:
+             ...     pprint(asset_property)
+
+        """
+        # payload = {}
+        # if asset_classes is not None:
+        #     asset_classes: str = ",".join([asset_class.value for asset_class in asset_classes])
+        #     payload["asset_classes"] = asset_classes
+        # asset_properties_response: dict[str, list[dict]] = self._get(path="one/api/v1/assets/properties", params=payload)
+        # return AssetProperties(**asset_properties_response).properties
+
+
+        return
--- tenable/one/assets/schema.py
+++ tenable/one/assets/schema.py
@@ -72,3 +72,4 @@ class AssetField(BaseModel):
 
 class AssetProperties(BaseModel):
     properties: list[AssetField]
+
