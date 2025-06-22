30b0b8cb0c2b355025e9885879742181e3210342
Shir Baruchian <shirush.bar@gmail.com>
2025-06-22 13:39:20 +0300

    [AI-24363] - Add get cards endpoint

--- tests/tenableone/exposure_view/cards/conftest.py
+++ tests/tenableone/exposure_view/cards/conftest.py
@@ -2,5 +2,5 @@ import pytest
 
 
 @pytest.fixture
-def cards(tenable_exposure_management_api):
-    return tenable_exposure_management_api.exposure_view.cards
+def cards(tenable_one_api):
+    return tenable_one_api.exposure_view.cards
