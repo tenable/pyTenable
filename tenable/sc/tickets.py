f5c9067f6ebcdac1680b1483012c66a58583bd92
Ken <Kenneth.Sager@nau.edu>
2025-12-30 13:46:02 -0700

    Tickets API (#975)
    * Adding Ticket API
    
    * Adding tests for tickets API. Minor changes to tickets.py
    
    * Adjust testing framework to python responses instead of pytest-vcr. Sonar suggestion for dict to literal.
    
    * addressing pylint findings

---
+++ tenable/sc/tickets.py
@@ -0,0 +1,191 @@
+'''
+tickets
+=====
+The following methods allow for interaction into the Tenable.sc
+:sc-api:`Ticket <Ticket.html>` API.  These items are typically seen under the
+Worklow --> Tickets section of Tenable.sc.
+Methods available on ``sc.tickets``:
+.. rst-class:: hide-signature
+.. autoclass:: TicketAPI
+    .. automethod:: create
+    .. automethod:: details
+    .. automethod:: edit
+    .. automethod:: list
+    Note: you cannot delete tickets, must set them to resolved and they will be auto-purged via system configured retention period
+'''
+from .base import SCEndpoint
+
+class TicketAPI(SCEndpoint):
+    def _constructor(self, **kw):
+        '''
+        Handles parsing the keywords and returns a ticket document
+        '''
+
+        # Validate as dict and pass to assignee
+        if 'assignee' in kw:
+            kw['assignee'] = self._check('assignee', kw['assignee'], dict)
+
+        # all of the following keys are string values and do not require any
+        # case conversion.  We will simply iterate through them and verify that
+        # they are in fact strings.
+        keys = ['name', 'description', 'notes']
+        for k in keys:
+            if k in kw:
+                self._check(k, kw[k], str)
+
+        if 'classification' in kw:
+            # Verify that classification is one of the correct possible values.
+            kw['classification'] = self._check(
+                'classification',
+                kw['classification'],
+                str,
+                choices=[
+                    'Information',
+                    'Configuration',
+                    'Patch',
+                    'Disable',
+                    'Firewall',
+                    'Schedule',
+                    'IDS',
+                    'Other',
+                    'Accept Risk',
+                    'Recast Risk',
+                    'Re-scan Request',
+                    'False Positive',
+                    'System Probe',
+                    'External Probe',
+                    'Investigation Needed',
+                    'Compromised System',
+                    'Virus Incident',
+                    'Bad Credentials',
+                    'Unauthorized Software',
+                    'Unauthorized System',
+                    'Unauthorized User'
+                ]
+            )
+
+        if 'status' in kw:
+            # Verify that status is one of the correct possible values
+            kw['status'] = self._check(
+                'status',
+                kw['status'],
+                str,
+                choices=[
+                    'Assigned',
+                    'Resolved',
+                    'More Information',
+                    'Not Applicable',
+                    'Duplicate',
+                    'Closed'
+                ]
+            )
+        return kw
+
+    def create(self, name, assignee, **kw):
+        '''
+        Creates a ticket.
+        :sc-api:`ticket: create <Ticket.html#ticket_POST>`
+        Args:
+            name (str):
+                Required: The name for the ticket
+            assignee (dict):
+                Required: A dictionary containing one key (id) for the user the ticket is being assigned to
+            status (str, optional):
+                Optional status of the ticket: assigned, resolved, etc.
+            classification (str, optional):
+                Optional classification of the ticket type, i.e. Information, Other, etc.
+            description (str, optional):
+                Optional description for the ticket
+            notes (str, optional):
+                Optional notes associated with the ticket
+            queries (list, optional):
+                Optional list of IDs of queries to associate with the ticket
+            query (object, optional):
+                Optional query object
+        Returns:
+            :obj:`dict`:
+                The newly created ticket.
+        Examples:
+            >>> ticket = sc.tickets.create('Example Ticket', {'id':1}, status='assigned', classification='information', description='This is an example ticket', notes='Example notes')
+        '''
+        kw['name'] = name
+        kw['assignee'] = assignee
+        payload = self._constructor(**kw)
+        return self._api.post('ticket', json=payload).json()['response']
+
+    def details(self, id, fields=None):
+        '''
+        Returns the details for a specific ticket.
+        :sc-api:`ticket: details <Ticket.html#TicketRESTReference-/ticket/{id}>`
+        Args:
+            id (int): 
+                Required: the unique identifier for the ticket to be returned
+            fields (list): 
+                An optional list of attributes to return.
+        Returns:
+            :obj:`dict`:
+                The ticket resource record.
+        Examples:
+            >>> ticket = sc.tickets.details(1)
+            >>> pprint(ticket)
+        '''
+        params = {}
+        if fields:
+            params['fields'] = ','.join([self._check('field', f, str) for f in fields])
+        ticket_id = self._check('id', id, int)
+        return self._api.get(f'ticket/{ticket_id}', params=params).json()['response']
+
+    def edit(self, id, **kw):
+        '''
+        Edits a ticket.
+        :sc-api:`ticket: edit <Ticket.html#ticket_id_PATCH>`
+        Args:
+            id (int):
+                Required: unique identifier of the ticket to be edited
+            name (str):
+                Optional name for the ticket. Must not be blank.
+            assignee (dict):
+                Optional dictionary containing one key (id) for the user the ticket is being assigned to
+            status (str, optional):
+                Optional status of the ticket: assigned, resolved, etc. Must not be blank.
+            classification (str, optional):
+                Optional classification of the ticket type, i.e. Information, Other, etc. Must not be blank.
+            description (str, optional):
+                Optional description for the ticket
+            notes (str, optional):
+                Optional notes associated with the ticket
+            queries (list, optional):
+                Optional list of IDs of queries to associate with the ticket
+            query (object, optional):
+                Optional query object
+
+        Returns:
+            :obj:`dict`:
+                The newly updated ticket.
+        Examples:
+            >>> ticket = sc.tickets.edit(1, status='Resolved', notes='ran updates')
+        '''
+        payload = self._constructor(**kw)
+        ticket_id = self._check('id', id, int)
+        return self._api.patch(f'ticket/{ticket_id}', json=payload).json()['response']
+
+    def list(self, fields=None):
+        '''
+        Outputs a dictionary of usable and manageable tickets, within which is a list of tickets.
+        :sc-api:`ticket: list <Ticket.html#ticket_GET>`
+        Args:
+            fields (list):
+                Optional list of attributes to return for each ticket, e.g. ["name","description"]. If not specified, only a list of ticket IDs will return
+        Returns:
+            :obj:`dict`:
+                A dictionary with two lists of ticket resources.
+        Examples:
+            >>> for ticket in sc.tickets.list():
+            ...     pprint(ticket)
+        '''
+        params = {}
+        if fields:
+            params['fields'] = ','.join([self._check('field', f, str)
+                for f in fields])
+
+        return self._api.get('ticket', params=params).json()['response']
---
+++ tests/sc/test_tickets.py
@@ -0,0 +1,265 @@
+"""
+test file for testing various scenarios in tickets
+"""
+import re
+import responses
+
+RE_BASE = (
+    r'https://nourl/rest'
+)
+
+
+def test_contructor(tsc):
+    '''
+    test tickets contstructor
+    '''
+    tickets = tsc.tickets._constructor(
+        assignee={'id': 0},
+        name='test',
+        description='test123',
+        notes='additional notes',
+        classification='Information',
+        status='Resolved'
+    )
+    assert isinstance(tickets, dict)
+    assert tickets['assignee'] == {'id': 0}
+    assert tickets['name'] == 'test'
+    assert tickets['description'] == 'test123'
+    assert tickets['notes'] == 'additional notes'
+    assert tickets['classification'] == 'Information'
+    assert tickets['status'] == 'Resolved'
+
+
+@responses.activate
+def test_tickets_list(tsc):
+    '''
+    test listing tickets
+    '''
+    responses.add(
+        responses.GET,
+        re.compile(f'{RE_BASE}/ticket'),
+        json={
+            "type" : "regular",
+            "response" : {
+                "usable" : [
+                    {
+                        "id" : "1",
+                        "name" : "TestTicket",
+                        "description" : ""            }
+                ],
+                "manageable" : [
+                    {
+                        "id" : "1",
+                        "name" : "TestTicket",
+                        "description" : ""            }
+                ]
+            },
+            "error_code" : 0,
+            "error_msg" : "",
+            "warnings" : {},
+            "timestamp" : 1423499298
+        }
+    )
+    tickets = tsc.tickets.list(fields={'id': 1})
+    assert isinstance(tickets, dict)
+    assert isinstance(tickets['usable'], list)
+    assert len(tickets['usable']) > 0
+    assert isinstance(tickets['usable'][0], dict)
+    assert tickets['usable'][0]['id'] == '1'
+
+
+@responses.activate
+def test_tickets_create(tsc):
+    '''
+    test creating a ticket
+    '''
+    responses.add(
+        responses.POST,
+        re.compile(f'{RE_BASE}/ticket'),
+        json={
+            "type" : "regular",
+            "response" : {
+                "id" : "1",
+                "name" : "test",
+                "description" : "Test",
+                "classification" : "Unauthorized System",
+                "status" : "assigned",
+                "notes" : "Created for testing of alerts",
+                "assignedTime" : "1424810461",
+                "resolvedTime" : "-1",
+                "closedTime" : "-1",
+                "createdTime" : "1424810461",
+                "modifiedTime" : "1424810461",
+                "queries" : [],
+                "canUse" : "true",
+                "canManage" : "true",
+                "canRespond" : "true",
+                "creator" : {
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "Security Manager",
+                    "lastname" : "",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     },
+                "owner" : {
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "Security Manager",
+                    "lastname" : "",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     },
+                "assignee" : {
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "Security Manager",
+                    "lastname" : "",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     },
+                "ownerGroup" : {
+                    "id" : "0",
+                    "name" : "Full Access",
+                    "description" : "Full Access group"     },
+                "assigneeGroup" : {
+                    "id" : "0",
+                    "name" : "Full Access",
+                    "description" : "Full Access group"     }
+            },
+            "error_code" : 0,
+            "error_msg" : "",
+            "warnings" : [],
+            "timestamp" : 1426879889
+        }
+    )
+    tickets = tsc.tickets.create(
+        name="test",
+        assignee={
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "Security Manager",
+                    "lastname" : "",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     }
+        )
+    assert isinstance(tickets, dict)
+
+
+@responses.activate
+def test_tickets_details(tsc):
+    '''
+    test getting a specific test
+    '''
+    responses.add(
+        responses.GET,
+        re.compile(f'{RE_BASE}/ticket/1'),
+        json={
+            "type" : "regular",
+            "response" : {
+                "id" : "1",
+                "name" : "TestTicket",
+                "description" : "",
+                "classification" : "Information",
+                "status" : "assigned",
+                "notes" : "",
+                "assignedTime" : "1423501383",
+                "resolvedTime" : "-1",
+                "closedTime" : "-1",
+                "createdTime" : "1423501383",
+                "modifiedTime" : "1423501383",
+                "canUse" : "true",
+                "canManage" : "true",
+                "canRespond" : "true",
+                "creator" : {
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "hi",
+                    "lastname" : "User",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     },
+                "owner" : {
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "hi",
+                    "lastname" : "User",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     },
+                "assignee" : {
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "hi",
+                    "lastname" : "User",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     },
+                "ownerGroup" : {
+                    "id" : "0",
+                    "name" : "Full Access",
+                    "description" : "Full Access group"     },
+                "assigneeGroup" : {
+                    "id" : "0",
+                    "name" : "Full Access",
+                    "description" : "Full Access group"     }
+            },
+            "error_code" : 0,
+            "error_msg" : "",
+            "warnings" : {},
+            "timestamp" : 1423501383
+        }
+    )
+    ticket = tsc.tickets.details(id=1, fields={'classification': 'Information'})
+    assert isinstance(ticket, dict)
+    assert ticket['classification'] == 'Information'
+
+
+@responses.activate
+def test_tickets_edit(tsc):
+    '''
+    testing editing a ticket
+    '''
+    responses.add(
+        responses.PATCH,
+        re.compile(f'{RE_BASE}/ticket/1'),
+        json={
+            "type" : "regular",
+            "response" : {
+                "id" : "1",
+                "name" : "TestTicket",
+                "description" : "",
+                "classification" : "False Positive",
+                "status" : "assigned",
+                "notes" : "",
+                "assignedTime" : "1423501383",
+                "resolvedTime" : "-1",
+                "closedTime" : "-1",
+                "createdTime" : "1423501383",
+                "modifiedTime" : "1423501383",
+                "canUse" : "true",
+                "canManage" : "true",
+                "canRespond" : "true",
+                "creator" : {
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "hi",
+                    "lastname" : "User",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     },
+                "owner" : {
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "hi",
+                    "lastname" : "User",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     },
+                "assignee" : {
+                    "id" : "1",
+                    "username" : "head",
+                    "firstname" : "hi",
+                    "lastname" : "User",
+                    "uuid" : "96F2AD1B-1B83-462E-908A-84E6054F6B64"     },
+                "ownerGroup" : {
+                    "id" : "0",
+                    "name" : "Full Access",
+                    "description" : "Full Access group"     },
+                "assigneeGroup" : {
+                    "id" : "0",
+                    "name" : "Full Access",
+                    "description" : "Full Access group"     }
+            },
+            "error_code" : 0,
+            "error_msg" : "",
+            "warnings" : {},
+            "timestamp" : 1423501383
+        }
+    )
+    ticket = tsc.tickets.edit(id=1, classification="False Positive")
+    assert isinstance(ticket, dict)
+    assert ticket['classification'] == 'False Positive'
