90746257b9d4060e62a8a522572d405f0dc0c6a3
Steven McGrath <smcgrath@tenable.com>
2026-06-09 17:07:00 -0500

    Corrected Regression created from scrub utility #1006 (#1007)

--- tenable/utils.py
+++ tenable/utils.py
@@ -1,3 +1,4 @@
+import logging
 import string
 import warnings
 from typing import Any
@@ -12,6 +13,9 @@ from tenable.base._restfly_v1 import (
     url_validator,
 )
 from tenable.base._restfly_v1 import dict_merge as _dm
+
+logger = logging.getLogger(__name__)
+
 
 __all__ = [
     'check',
@@ -85,5 +89,10 @@ def scrub(value: Any) -> str:
     """
     Scrubs converts the value to a string and then scrubs out any illegal characters.
     """
-    safe_chars = string.ascii_letters + string.digits + '-_%@'
-    return ''.join([c for c in str(value) if c in safe_chars])
+    safe_chars = string.ascii_letters + string.digits + '-_%@:'
+    scrubbed_value = ''.join([c for c in str(value) if c in safe_chars])
+    if value != scrubbed_value:
+        logger.warning(
+            f"Value '{value}' has unsafe chars, scrubbing to '{scrubbed_value}'"
+        )
+    return scrubbed_value
